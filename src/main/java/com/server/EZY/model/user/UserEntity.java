3f827009ecb04b456ea128dafd88d6d8b92b9bbf
siwony_ <siwon103305@gmail.com>
2021-05-27 20:04:18 +0900

    [ADD] UserEntity column mapping finish releationship not yet

--- src/main/java/com/server/EZY/model/user/UserEntity.java
+++ src/main/java/com/server/EZY/model/user/UserEntity.java
@@ -1,2 +1,34 @@
-package com.server.EZY.model.user;public class UserEntity {
+package com.server.EZY.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserId")
+    private Long userIdx;
+
+    @Column(name = "NickName")
+    private String nickname;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "PhoneNumber")
+    private String phoneNumber;
+
+    @Column(name = "Permission")
+    @Enumerated(value = EnumType.STRING)
+    private Permission permission;
+
 }
