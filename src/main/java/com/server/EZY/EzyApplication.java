d22c0b999b4bdb9171bb1d728c5e373d02f1412c
johnjihwan <s20062@gsm.hs.kr>
2021-05-27 14:06:03 +0900

    add basic dependency and env

---
+++ src/main/java/com/server/EZY/EzyApplication.java
@@ -0,0 +1,13 @@
+package com.server.EZY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzyApplication.class, args);
+	}
+
+}
