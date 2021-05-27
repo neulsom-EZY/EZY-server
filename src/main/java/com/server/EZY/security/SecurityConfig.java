e05cf775841c0814b93f8e055ab9cbebd1837374
johnjihwan <s20062@gsm.hs.kr>
2021-05-27 17:23:38 +0900

    Update springboot best file structure

--- src/main/java/com/server/EZY/security/SecurityConfig.java
+++ src/main/java/com/server/EZY/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.server.EZY.configuration.security;
+package com.server.EZY.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
