ba0bb5175293c912acbf35b9dc9349f9dd1d50e5
johnjihwan <s20062@gsm.hs.kr>
2021-05-28 14:02:53 +0900

    JWT 시크릿키 검증 완료

--- src/test/java/com/server/EZY/EzyApplicationTests.java
+++ src/test/java/com/server/EZY/EzyApplicationTests.java
@@ -3,14 +3,23 @@ package com.server.EZY;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EzyApplicationTests {
 
+	@Value("${security.jwt.token.secret-key}")
+	private String jwtKey;
+
 	@Test
 	void JWT_SECRET_KEY_검증() {
+		// Given
+		String expectValue = "afsdasfdASAWRSasgdvFTUJARGtasdfSRTJSRTEQR14asdfVA";
+
+		// Then
+		System.out.println("기대하는 값은= "+expectValue);
+		assertThat(jwtKey.equals(expectValue));
 	}
 
 }
