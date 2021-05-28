5aa4bc8aaf6eda8d8a6d40eb851e6400deb4187a
siwony_ <siwon103305@gmail.com>
2021-05-28 09:28:50 +0900

    [ADD] UserEntity repository

--- src/main/java/com/server/EZY/repository/UserRepository.java
+++ src/main/java/com/server/EZY/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.server.EZY.repository;public interface UserRepo {
-}
+package com.server.EZY.repository;
+
+import com.server.EZY.model.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> { }
