e1ca91365a4c549e1b4d6874e3452ce062e5fbd5
johnjihwan <s20062@gsm.hs.kr>
2021-05-29 18:25:11 +0900

    bean fuxxing shit error solved

--- src/main/java/com/server/EZY/security/SecurityConfig.java
+++ src/main/java/com/server/EZY/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.server.EZY.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
