4c365cf231d8b725c7464335a2e2f229d2c7dfab
johnjihwan <s20062@gsm.hs.kr>
2021-05-29 17:59:06 +0900

    fusxxz

--- src/main/java/com/server/EZY/security/JwtTokenFilter.java
+++ src/main/java/com/server/EZY/security/JwtTokenFilter.java
@@ -1,7 +1,6 @@
 package com.server.EZY.security;
 
 import com.server.EZY.exception.CustomException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
