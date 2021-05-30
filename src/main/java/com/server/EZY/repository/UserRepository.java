d56394b5c789360b290db97f041b763fa30ed365
johnjihwan <s20062@gsm.hs.kr>
2021-05-30 20:39:06 +0900

    userdetails service를 구현하기 위한 findBy() method 추가

--- src/main/java/com/server/EZY/repository/UserRepository.java
+++ src/main/java/com/server/EZY/repository/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> { }
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByNickname(String nickName);
+}
