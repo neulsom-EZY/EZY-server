97e50e773f3a7d1663b70b416f13e3380bc1a04e
johnjihwan <s20062@gsm.hs.kr>
2021-05-30 20:51:27 +0900

    현제 닉네임이 존재하는지 아닌지 검증하는 불린형 선언

--- src/main/java/com/server/EZY/repository/UserRepository.java
+++ src/main/java/com/server/EZY/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByNickname(String nickName);
+    boolean existsByNickname(String nickname);
 }
