9156bf76881d8e8ef21c35f9d290f130460bf755
johnjihwan <s20062@gsm.hs.kr>
2021-05-30 20:38:31 +0900

    userdetails service adding

--- src/main/java/com/server/EZY/security/MyUserDetails.java
+++ src/main/java/com/server/EZY/security/MyUserDetails.java
@@ -1,2 +1,25 @@
-package com.server.EZY.security;public class MyUserDetails {
+package com.server.EZY.security;
+
+import com.server.EZY.model.user.UserEntity;
+import com.server.EZY.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetails implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByNickname(nickname);
+
+        if(nickname == null){
+            throw new UsernameNotFoundException("nickName '" + nickname + "' not found");
+        }
+
+        return userEntity;
+    }
 }
