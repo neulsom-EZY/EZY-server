0431c33efb933d4d519b2c1edce81b03ea6ff94b
johnjihwan <s20062@gsm.hs.kr>
2021-05-30 20:55:51 +0900

    user dto 추가

---
+++ src/main/java/com/server/EZY/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.server.EZY.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+}
