69a623c9a812e17cd9bc9291061faab2a45348a3
johnjihwan <s20062@gsm.hs.kr>
2021-05-31 14:01:38 +0900

    testcode 작성하기 전 초안 service class 작성

--- src/main/java/com/server/EZY/service/UserService.java
+++ src/main/java/com/server/EZY/service/UserService.java
@@ -1,2 +1,28 @@
-package com.server.EZY.service;public class UserService {
+package com.server.EZY.service;
+
+import com.server.EZY.dto.UserDto;
+import com.server.EZY.repository.UserRepository;
+import com.server.EZY.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    private JwtTokenProvider jwtTokenProvider;
+    private AuthenticationManager authenticationManager;
+
+    public String signin(){
+        return null;
+    }
+    public String signup(UserDto userDto){
+        if(!userRepository.existsByNickname(userDto.getNickname())){
+
+        }
+        return null;
+    }
 }
