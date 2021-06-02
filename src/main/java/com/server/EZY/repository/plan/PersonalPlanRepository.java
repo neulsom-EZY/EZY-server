4bbacebaa0aaba2aac93aa2f4e84ab04a4ef09a5
siwony_ <siwon103305@gmail.com>
2021-06-02 13:40:15 +0900

    [FIX] JpaRepository <PersonalPlanRepository, Long> -> PersonalPlanRepository<PersonalPlanEntity, Long>

--- src/main/java/com/server/EZY/repository/plan/PersonalPlanRepository.java
+++ src/main/java/com/server/EZY/repository/plan/PersonalPlanRepository.java
@@ -1,8 +1,9 @@
 package com.server.EZY.repository.plan;
 
+import com.server.EZY.model.plan.personal.PersonalPlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalPlanRepository extends JpaRepository<PersonalPlanRepository, Long> {
+public interface PersonalPlanRepository extends JpaRepository<PersonalPlanEntity, Long> {
 }
