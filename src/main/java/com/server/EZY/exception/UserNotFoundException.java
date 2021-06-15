ff1ed6311883a646bac300b65d4c0841f2fbabd6
qoxogus <a01034367055@gmail.com>
2021-06-15 20:27:51 +0900

    [ADD] Exceptions

--- src/main/java/com/server/EZY/exception/UserNotFoundException.java
+++ src/main/java/com/server/EZY/exception/UserNotFoundException.java
@@ -1,2 +1,13 @@
-package com.server.EZY.exception;public class UserNotFoundException {
+package com.server.EZY.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String msg, Throwable t){
+        super(msg, t);
+    }
+    public UserNotFoundException(String msg){
+        super(msg);
+    }
+    public UserNotFoundException(){
+        super();
+    }
 }
