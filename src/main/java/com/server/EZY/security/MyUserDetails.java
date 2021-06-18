42c43742c173b50791ee851369e504e66dcc2843
qoxogus <a01034367055@gmail.com>
2021-06-18 13:37:54 +0900

    [FIX] 생성자 주입으로 변경

--- src/main/java/com/server/EZY/security/MyUserDetails.java
+++ src/main/java/com/server/EZY/security/MyUserDetails.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MyUserDetails implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
