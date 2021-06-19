44be9c5288095621fa536b0567c9f0518c54b3d0
siwony_ <siwon103305@gmail.com>
2021-06-20 05:04:04 +0900

    [UPDATE] planRepository findAllByUserEntityAndPersonalPlanEntityNotNull 추가

--- src/main/java/com/server/EZY/repository/plan/PlanRepository.java
+++ src/main/java/com/server/EZY/repository/plan/PlanRepository.java
@@ -1,9 +1,14 @@
 package com.server.EZY.repository.plan;
 
+import com.server.EZY.model.plan.PlanDType;
 import com.server.EZY.model.plan.PlanEntity;
+import com.server.EZY.model.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlanRepository extends JpaRepository<PlanEntity, Long> {
+    List<PlanEntity> findAllByUserEntityAndPersonalPlanEntityNotNull(UserEntity userEntity);
 }
