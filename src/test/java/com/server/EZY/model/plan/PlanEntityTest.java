88f7d71abbc86470cfd553e20a3fd3883a6899a6
siwony_ <siwon103305@gmail.com>
2021-06-19 15:47:54 +0900

    [UPDATE] test 편의를 위한 personalPlanEntityInit method 추가

--- src/test/java/com/server/EZY/model/plan/PlanEntityTest.java
+++ src/test/java/com/server/EZY/model/plan/PlanEntityTest.java
@@ -1,19 +1,35 @@
 package com.server.EZY.model.plan;
 
+import com.server.EZY.model.plan.personal.PersonalPlanEntity;
+import com.server.EZY.model.user.Permission;
+import com.server.EZY.model.user.Role;
+import com.server.EZY.model.user.UserEntity;
 import com.server.EZY.repository.plan.PersonalPlanRepository;
 import com.server.EZY.repository.plan.PlanRepository;
 import com.server.EZY.repository.plan.TeamPlanRepository;
+import org.assertj.core.internal.bytebuddy.utility.RandomString;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 @DataJpaTest
 class PlanEntityTest {
 
-    @Autowired PlanRepository PlanRepo;
+    @Autowired PlanRepository planRepo;
     @Autowired PersonalPlanRepository personalPlanRepo;
     @Autowired TeamPlanRepository teamPlanRepo;
+
+    PersonalPlanEntity personalPlanEntityInit(){
+        return PersonalPlanEntity.builder()
+                .planName(RandomString.make(10))
+                .when(Calendar.getInstance())
+                .where(RandomString.make(20))
+                .repeat(false)
+                .build();
+    }
 
 
 }
