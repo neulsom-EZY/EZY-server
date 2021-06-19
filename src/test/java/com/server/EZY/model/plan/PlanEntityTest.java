f3cbe3f0dff1d65a9bb472fcd2c2bf4afa98c55e
siwony_ <siwon103305@gmail.com>
2021-06-19 16:21:52 +0900

    [UPDATE] PersonalPlan 생성 및 저장 테스트 완료

--- src/test/java/com/server/EZY/model/plan/PlanEntityTest.java
+++ src/test/java/com/server/EZY/model/plan/PlanEntityTest.java
@@ -7,6 +7,7 @@ import com.server.EZY.model.user.UserEntity;
 import com.server.EZY.repository.plan.PersonalPlanRepository;
 import com.server.EZY.repository.plan.PlanRepository;
 import com.server.EZY.repository.plan.TeamPlanRepository;
+import com.server.EZY.repository.user.UserRepository;
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +16,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 class PlanEntityTest {
 
+    @Autowired UserRepository userRepo;
     @Autowired PlanRepository planRepo;
     @Autowired PersonalPlanRepository personalPlanRepo;
     @Autowired TeamPlanRepository teamPlanRepo;
@@ -25,6 +29,7 @@ class PlanEntityTest {
     PersonalPlanEntity personalPlanEntityInit(){
         return PersonalPlanEntity.builder()
                 .planName(RandomString.make(10))
+                .what(RandomString.make(20))
                 .when(Calendar.getInstance())
                 .where(RandomString.make(20))
                 .repeat(false)
@@ -42,4 +47,33 @@ class PlanEntityTest {
     }
 
 
+    @Test @DisplayName("PersonalPlan 생성 및 저장 테스트")
+    void PersonalPlan를_통해_PlanEntity생성_검증(){
+        // Given
+        PersonalPlanEntity personalPlanEntity = personalPlanEntityInit();
+        UserEntity userEntity = userEntityInit();
+        userRepo.save(userEntity);
+
+        List<String> categories = Collections.singletonList("공부");
+        PlanEntity planEntity = new PlanEntity(
+                personalPlanEntity
+                ,userEntity
+                ,categories
+        );
+
+        // When
+        PlanEntity savedPlanEntity = planRepo.save(planEntity);
+
+        UserEntity getUserEntity = savedPlanEntity.getUserEntity();
+        PlanDType getPlanDType = savedPlanEntity.getPlanDType();
+        PersonalPlanEntity getPersonalPlanEntity = savedPlanEntity.getPersonalPlanEntity();
+        List<String> getCategories = savedPlanEntity.getCategories();
+
+
+        // Then
+        assertEquals(getUserEntity, userEntity);
+        assertEquals(getPlanDType, PlanDType.PERSONAL_PLAN);
+        assertEquals(getPersonalPlanEntity, personalPlanEntity);
+        assertEquals(getCategories.get(0), categories.get(0));
+    }
 }
