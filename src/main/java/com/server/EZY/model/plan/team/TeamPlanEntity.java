03841de281391a73e67d24f74262fc4745b693e7
siwony_ <siwon103305@gmail.com>
2021-06-24 14:22:52 +0900

    [UPDATE] TeamPlanEntity의 updateTeamPlan 매서드 주석

--- src/main/java/com/server/EZY/model/plan/team/TeamPlanEntity.java
+++ src/main/java/com/server/EZY/model/plan/team/TeamPlanEntity.java
@@ -37,13 +37,27 @@ public class TeamPlanEntity {
     @Column(name = "PlanWhere")
     private String where;
 
-    public void updateTeamPlan(TeamPlanEntity updatedTeamPlanEntity, UserEntity teamLeader) throws Exception {
+    /**
+     * 현재 TeamPlanEntity의 필드값을 업데이트 하는 함수다.<br>
+     *
+     * 현재 Entity를 업데이트하기 전에 UserEntity타입의 teamLeader를 인수로 받아 현재 변경하려는 유저가 팀리더인지 체크한다. <br>
+     * 만약 팀리더가 아닌 다른 유저가 TeamPlanEntity를 변경하려 하는 경우 Exception(임시)이 발생한다. <br>
+     *
+     * 현재 Entity의 값을 바꾸기 위해 TeamPlanEntity타입의 updatedTeamPlanEntity를 인수로 받아 <br>
+     * updatedTeamPlanEntity 안에 있는 각각의 필드의 값이 null이 아니면 현재 Entity의 값을 변경한다. <br>
+     *
+     * @param updateTeamPlanEntity 바꾸고 싶은 필드의 값이 들어있는 TeamPlanEntity 타입의 매개변수
+     * @param currentUser Team 리더를 확인하기 위한 UserEntity 타입의 매개변수
+     * @throws Exception Team 리더가 아닌 다른 유저가 현재 TeamPlanEntity를 수정하려는 경우 Exception(임시)
+     * @author 정시원
+     */
+    public void updateTeamPlan(TeamPlanEntity updateTeamPlanEntity, UserEntity currentUser) throws Exception {
         // 팀리더만 검증
-        if(this.teamLeader.getUsername().equals(teamLeader.getUsername())){
-            this.what = updatedTeamPlanEntity.what != null ? updatedTeamPlanEntity.what : this.what;
-            this.planName = updatedTeamPlanEntity.planName != null ? updatedTeamPlanEntity.planName : this.planName;
-            this.when = updatedTeamPlanEntity.when != null ? updatedTeamPlanEntity.when : this.when;
-            this.where = updatedTeamPlanEntity.where != null ? updatedTeamPlanEntity.where : this.where;
+        if(this.teamLeader.getUsername().equals(currentUser.getUsername())){
+            this.what = updateTeamPlanEntity.what != null ? updateTeamPlanEntity.what : this.what;
+            this.planName = updateTeamPlanEntity.planName != null ? updateTeamPlanEntity.planName : this.planName;
+            this.when = updateTeamPlanEntity.when != null ? updateTeamPlanEntity.when : this.when;
+            this.where = updateTeamPlanEntity.where != null ? updateTeamPlanEntity.where : this.where;
         }else
             throw new Exception("팀리더만 변경할 수 있습니다.");
     }
