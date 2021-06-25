4959527b7925fa665051657091dfc2d496052122
johnjihwan <s20062@gsm.hs.kr>
2021-06-26 03:49:35 +0900

    Add personalPlan 저장하는 서비스 로직을 작성했습니다.

--- src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
+++ src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
@@ -1,2 +1,19 @@
-package com.server.EZY.model.plan.personal.service;public class PersonalPlanService {
+package com.server.EZY.model.plan.personal.service;
+
+import com.server.EZY.model.plan.personal.dto.PersonalPlanDto;
+import com.server.EZY.model.plan.personal.repository.PersonalPlanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PersonalPlanService {
+    private final PersonalPlanRepository personalPlanRepository;
+
+    @Transactional
+    public String savePersonalPlan(PersonalPlanDto myPersonalPlan){
+        personalPlanRepository.save(myPersonalPlan.toEntity());
+        return myPersonalPlan.getPlanName() + "의 제목의 일정이 추가 되었습니다.";
+    }
 }
