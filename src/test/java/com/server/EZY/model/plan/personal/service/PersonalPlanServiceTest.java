a6408061d8f8bd68d8af74596507f946e60f7c74
johnjihwan <s20062@gsm.hs.kr>
2021-06-26 03:50:04 +0900

    Add personalPlan - 저장 서비스 로직이 잘 작동하는지 검증하는 TestCode를 작성하였습니다.

--- src/test/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceTest.java
+++ src/test/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceTest.java
@@ -1,4 +1,54 @@
+package com.server.EZY.model.plan.personal.service;
+
+import com.server.EZY.model.plan.personal.dto.PersonalPlanDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
 class PersonalPlanServiceTest {
-  
+
+    @Autowired
+    private PersonalPlanService personalPlanService;
+
+    @BeforeEach
+    public void 개인일정세트(){
+        PersonalPlanDto myPersonalPlan = PersonalPlanDto.builder()
+                .planName("지환이랑 놀기")
+                .when(Calendar.getInstance())
+                .where("롯데월드")
+                .what("리프레시 타임")
+                .who("시원이, 태현이")
+                .repeat(false)
+                .build();
+    }
+
+    @Test
+    public void 개인일정추가(){
+        // Given
+        PersonalPlanDto myPersonalPlan = PersonalPlanDto.builder()
+                .planName("지환이랑 놀기")
+                .when(Calendar.getInstance())
+                .where("롯데월드")
+                .what("리프레시 타임")
+                .who("시원이, 태현이")
+                .repeat(false)
+                .build();
+
+        //When
+        String result = personalPlanService.savePersonalPlan(myPersonalPlan);
+
+        //Then
+        assertEquals("지환이랑 놀기의 제목의 일정이 추가 되었습니다.", result);
+        System.out.println("============== result = "+ result + " ===================");
+    }
 }
