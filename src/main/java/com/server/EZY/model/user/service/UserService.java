e7ccfc15b7348f013e1a1f07df18fdf418c552cb
johnjihwan <s20062@gsm.hs.kr>
2021-06-26 02:56:50 +0900

    Update userService javadoc edit

--- src/main/java/com/server/EZY/model/user/service/UserService.java
+++ src/main/java/com/server/EZY/model/user/service/UserService.java
@@ -29,6 +29,7 @@ public class UserService {
      * @param userDto
      * @return - if, save 완료 시 token return.
      * @exception - else, 이미 존재하면 userAlreadyExist 터트리기.
+     * @author 전지환
      */
     public String signup(UserDto userDto) {
         if(!userRepository.existsByNickname(userDto.getNickname())){
@@ -47,8 +48,9 @@ public class UserService {
      * 로그인을 하는 서비스 로직 입니다.
      * @param loginDto
      * @exception 1. email을 통해 회원을 찾을 수 있나요? -> false, UserNotFoundException();
-     *            2. 해당 회원의 비밀번호가 loginDto.getPassword() 와 일치하나요? -> false, UserNotFoundException();
+     *            2. 해당 회원의 비밀번호가 loginDto.getPassword()와 일치하나요? -> false, UserNotFoundException();
      * @return 서두에 있는 모든 조건을 만족할 시에  Map<String ,String> 을 반환 합니다.
+     * @author 전지환
      */
     public Map<String, String> signin(LoginDto loginDto){
         UserEntity findUser = userRepository.findByNickname(loginDto.getNickname());
