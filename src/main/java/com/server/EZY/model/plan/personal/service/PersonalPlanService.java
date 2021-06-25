67e4638f8a8af29d583ca7faa34c2d17be8e4883
johnjihwan <s20062@gsm.hs.kr>
2021-06-26 04:30:20 +0900

    Add this commit is draft code commit! -> 개인일정 업데이트 메서드 초안을 작성했습니다.

--- src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
+++ src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
@@ -1,10 +1,13 @@
 package com.server.EZY.model.plan.personal.service;
 
+import com.server.EZY.model.plan.personal.PersonalPlanEntity;
 import com.server.EZY.model.plan.personal.dto.PersonalPlanDto;
+import com.server.EZY.model.plan.personal.dto.PersonalPlanUpdateDto;
 import com.server.EZY.model.plan.personal.repository.PersonalPlanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,16 @@ public class PersonalPlanService {
     public String savePersonalPlan(PersonalPlanDto myPersonalPlan){
         personalPlanRepository.save(myPersonalPlan.toEntity());
         return myPersonalPlan.getPlanName() + "의 제목의 일정이 추가 되었습니다.";
+    }
+
+    @Transactional
+    public String updatePersonalPlan(PersonalPlanUpdateDto myPersonalPlanUpdate, Long personalPlanIdx) throws Exception {
+        PersonalPlanEntity updateTarget = personalPlanRepository.findByPersonalPlanIdx(personalPlanIdx);
+        if (updateTarget != null) {
+            updateTarget.updatePersonalPlan(myPersonalPlanUpdate.toEntity());
+            return myPersonalPlanUpdate.getPlanName() + "의 제목의 일정으로 변경되었습니다.";
+        } else {
+            throw new Exception("변경하고자 하는 일정이 존재하지 않습니다.");
+        }
     }
 }
