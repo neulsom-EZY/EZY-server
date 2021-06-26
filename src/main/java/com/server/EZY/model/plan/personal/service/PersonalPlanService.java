022a73e56efbbb8ff5ee830803899d10d0b72d4f
johnjihwan <s20062@gsm.hs.kr>
2021-06-27 02:03:01 +0900

    Refectoring select userEntity method

--- src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
+++ src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanService.java
@@ -33,10 +33,8 @@ public class PersonalPlanService {
      */
     @Transactional
     public PlanEntity savePersonalPlan(PersonalPlanDto myPersonalPlan, List<String> personalPlanCategory){
-        // loginUserNickname 은 userService 에 Public static 으로 선언 된 method를 사용합니다.
-        String loginUserNickname = UserServiceImpl.getCurrentUserNickname();
-        // currentUserEntity 를 사용하여 loginUserNickname 으로 찾은 Entity 를 저장합니다.
-        UserEntity loginUserEntity = currentUserEntity(loginUserNickname);
+        // 로그인된 userEntity를 불러옵니다.
+        UserEntity loginUserEntity = currentUserEntity();
         // return 할 savedPlanEntity 필드를 메서드 내에서 전역 선언 합니다.
         PlanEntity savedPlanEntity;
 
@@ -64,23 +62,16 @@ public class PersonalPlanService {
         return savedPlanEntity;
     }
 
-    @Transactional
-    public String updatePersonalPlan(PersonalPlanUpdateDto myPersonalPlanUpdate, Long personalPlanIdx) throws Exception {
-        PersonalPlanEntity updateTarget = personalPlanRepository.findByPersonalPlanIdx(personalPlanIdx);
-        if (updateTarget != null) {
-            updateTarget.updatePersonalPlan(myPersonalPlanUpdate.toEntity());
-            return myPersonalPlanUpdate.getPlanName() + "의 제목의 일정으로 변경되었습니다.";
-        } else {
-            throw new Exception("변경하고자 하는 일정이 존재하지 않습니다.");
-        }
-    }
+//    public List<PlanEntity> getPersonalPlanAll(){
+//
+//    }
 
     /**
      * 이 메서드는 임시적으로 선언한 메서드이며, userNickname을 통해 Entity를 찾아 return 해줍니다.
-     * @param loginUserNickname
      * @return UserEntity
      */
-    public UserEntity currentUserEntity(String loginUserNickname){
+    public UserEntity currentUserEntity(){
+        String loginUserNickname = UserServiceImpl.getCurrentUserNickname();
         return userRepository.findByNickname(loginUserNickname);
     }
 }
