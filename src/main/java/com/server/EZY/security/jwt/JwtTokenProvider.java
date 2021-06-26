b959877201fd27aa6ac23239f5ffd06143ad50ff
qoxogus <a01034367055@gmail.com>
2021-06-26 13:40:55 +0900

    [ADD] 주석 추가

--- src/main/java/com/server/EZY/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/server/EZY/security/jwt/JwtTokenProvider.java
@@ -23,18 +23,37 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.secretKey}")
     private String secretKey;
 
-    private long TOKEN_VALIDATION_SECOND = 360000; // 1h  360000
-    private long REFRESH_TOKEN_VALIDATION_TIME = TOKEN_VALIDATION_SECOND * 24 * 180; //6months
+    /**
+     * accessToken의 만료기간 (1시간 - 1hour)
+     * @author 배태현
+     */
+    private long TOKEN_VALIDATION_SECOND = 360000;
+
+    /**
+     * refreshToken의 만료기간 (6달 - 6month)
+     * @author 배태현
+     */
+    private long REFRESH_TOKEN_VALIDATION_TIME = TOKEN_VALIDATION_SECOND * 24 * 180;
 
     private final MyUserDetails myUserDetails;
 
-    // Base64 encoded secret key
+    /**
+     * secretKey를 Base64인코딩방식을 사용하여 인코딩하는 메서드
+     * @author 배태현
+     */
     @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
-    //token 생성
+    /**
+     * 사용자의 이름과 권한을 이용해서 accessToken을 만드는 메소드입니다.
+     *
+     * @param username 사용자의 이름
+     * @param roles 사용자 권한 (ROLE_ADMIN, ROLE_CLIENT, ROLE_NOT_PERMIT)
+     * @return accessToken
+     * @author 배태현
+     */
     public String createToken(String username, List<Role> roles){
         Claims claims = Jwts.claims().setSubject(username);
         claims.put("auth", roles.stream().
@@ -52,6 +71,11 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    /**
+     * refreshToken을 만드는 메소드이며 보안상의 이유로 claim에 아무정보도 들어가지 않습니다.
+     * @return refreshToken
+     * @author 배태현
+     */
     public String createRefreshToken(){
         Claims claims = Jwts.claims().setSubject(null);
 
@@ -66,18 +90,33 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    // token에서 인증정보 조회
+    /**
+     * 사용자의 인증정보를 조회하는 메소드입니다.
+     * @param token 사용자의 인증정보를 조회 할 token
+     * @return 인증정보 (true || false)
+     * @author 배태현
+     */
     public Authentication getAuthentication(String token){
         UserDetails userDetails = myUserDetails.loadUserByUsername(getUsername(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    //token에서 회원 정보 추출
+    /**
+     * token을 복호화 하여 claim에서 사용자 이름(Username)을 가져오는 메소드입니다.
+     * @param token Username을 추출 할 token
+     * @return username
+     * @author 배태현
+     */
     public String getUsername(String token){
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
-    // header(Authorization)에서 token 값 가져오기
+    /**
+     * http header에서 accessToken을 가져오는 메소드입니다.
+     * @param req HttpServletRequest
+     * @return true = accesstoken, false = null
+     * @author 배태현
+     */
     public String resolveToken(HttpServletRequest req){
         String bearerToken = req.getHeader("Authorization");
         if(bearerToken != null && bearerToken.startsWith("Bearer ")){
@@ -86,6 +125,12 @@ public class JwtTokenProvider {
         return null;
     }
 
+    /**
+     * http header에서 refreshToken을 가져오는 메소드입니다.
+     * @param req HttpServletRequest
+     * @return true = refreshToken, false = null
+     * @author 배태현
+     */
     public String resolveRefreshToken(HttpServletRequest req){
         String refreshToken = req.getHeader("RefreshToken");
         if(refreshToken != null){
@@ -94,7 +139,12 @@ public class JwtTokenProvider {
         return null;
     }
 
-    //token 검증 (유효성, 만료일자 확인)
+    /**
+     * token을 검증하는 메소드 (유효성, 만료일자 검증)
+     * @param token 검증 할 token
+     * @return 토큰이 검증됨 = true, 토큰이 검증되지않음 = false
+     * @author 배태현
+     */
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
