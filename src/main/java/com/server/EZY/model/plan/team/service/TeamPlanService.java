c4ad48f8bb40c8c2d9220068a39ccb81b18da618
johnjihwan <s20062@gsm.hs.kr>
2021-07-04 00:08:13 +0900

    Add initial TeamPlanService logic

--- src/main/java/com/server/EZY/model/plan/team/service/TeamPlanService.java
+++ src/main/java/com/server/EZY/model/plan/team/service/TeamPlanService.java
@@ -1,2 +1,23 @@
-package com.server.EZY.model.plan.team.service;public class TeamPlanService {
+package com.server.EZY.model.plan.team.service;
+
+import com.server.EZY.model.plan.plan.PlanEntity;
+import com.server.EZY.model.plan.plan.repository.PlanRepository;
+import com.server.EZY.model.plan.team.dto.TeamPlanDto;
+import com.server.EZY.model.user.UserEntity;
+import com.server.EZY.model.user.util.CurrentUserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TeamPlanService {
+    private final PlanRepository planRepository;
+    private final CurrentUserUtil currentUserUtil;
+
+    @Transactional
+    public PlanEntity saveTeamPlan(TeamPlanDto teamPlanDto){
+        UserEntity getCurrentUser = currentUserUtil.getCurrentUser();
+        return null;
+    }
 }
