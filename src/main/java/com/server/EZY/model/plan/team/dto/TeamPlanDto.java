c8b845b4141b884110a90a85e28fada2f95761d3
johnjihwan <s20062@gsm.hs.kr>
2021-07-04 02:52:34 +0900

    TeamPlanDto 를 재정의 했습니다. 이유는 TeamLeader가 NotNull 처리 됐지만 insert 하는 Data가 없어 SQL statement 에러가 발생하였기 때문에.

--- src/main/java/com/server/EZY/model/plan/team/dto/TeamPlanDto.java
+++ src/main/java/com/server/EZY/model/plan/team/dto/TeamPlanDto.java
@@ -1,6 +1,8 @@
 package com.server.EZY.model.plan.team.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.server.EZY.model.plan.team.TeamPlanEntity;
+import com.server.EZY.model.user.UserEntity;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -10,6 +12,8 @@ import java.util.Calendar;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class TeamPlanDto {
+    @JsonIgnore
+    private UserEntity teamLeader;
     @NotBlank
     private String planName;
     private String what;
@@ -19,6 +23,7 @@ public class TeamPlanDto {
 
     public TeamPlanEntity toEntity(){
         return TeamPlanEntity.builder()
+                .teamLeader(this.teamLeader)
                 .planName(this.planName)
                 .what(this.what)
                 .when(this.when)
