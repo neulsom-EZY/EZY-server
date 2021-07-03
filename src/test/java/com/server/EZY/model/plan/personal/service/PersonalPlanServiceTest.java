77500bd375a938ed8c773e17648b3c9870dea5ae
johnjihwan <s20062@gsm.hs.kr>
2021-07-04 00:07:58 +0900

    Delete except import statement

--- src/test/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceTest.java
+++ src/test/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceTest.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
