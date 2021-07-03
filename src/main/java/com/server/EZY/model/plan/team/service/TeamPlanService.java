df7c46f1992ee15be011cd47d8ae59b4ad5f4bdf
johnjihwan <s20062@gsm.hs.kr>
2021-07-04 02:24:11 +0900

    Add teamPlanService 로직 초안 작성

--- src/main/java/com/server/EZY/model/plan/team/service/TeamPlanService.java
+++ src/main/java/com/server/EZY/model/plan/team/service/TeamPlanService.java
@@ -1,6 +1,7 @@
 package com.server.EZY.model.plan.team.service;
 
 import com.server.EZY.model.plan.headOfPlan.HeadOfPlanEntity;
+import com.server.EZY.model.plan.headOfPlan.repository.HeadOfPlanRepository;
 import com.server.EZY.model.plan.team.dto.TeamPlanDto;
 import com.server.EZY.model.user.UserEntity;
 import com.server.EZY.model.user.util.CurrentUserUtil;
@@ -11,11 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class TeamPlanService {
+    private final HeadOfPlanRepository headOfPlanRepository;
     private final CurrentUserUtil currentUserUtil;
 
     @Transactional
     public HeadOfPlanEntity saveTeamPlan(TeamPlanDto teamPlanDto){
+        // 현재 로그인 된 UserEntity 할당.
         UserEntity getCurrentUser = currentUserUtil.getCurrentUser();
-        return null;
+        // team 일정과 user를 세트.
+        HeadOfPlanEntity headOfPlanEntity = new HeadOfPlanEntity(
+                teamPlanDto.toEntity(),
+                getCurrentUser
+        );
+        return headOfPlanRepository.save(headOfPlanEntity);
     }
 }
