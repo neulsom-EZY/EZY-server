3f2fc6740cd8cf452d113b41b79d346bf1033ba6
siwony_ <siwon103305@gmail.com>
2021-07-05 17:18:01 +0900

    [ADD] TokenExceptionController 추가

--- src/main/java/com/server/EZY/exception/token/TokenExceptionController.java
+++ src/main/java/com/server/EZY/exception/token/TokenExceptionController.java
@@ -1,2 +1,22 @@
-package com.server.EZY.exception.token.exception;public class TokenExceptionController {
+package com.server.EZY.exception.token;
+
+import com.server.EZY.exception.token.exception.AccessTokenExpiredException;
+import com.server.EZY.exception.token.exception.InvalidTokenException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception")
+public class TokenExceptionController {
+
+    @GetMapping("/access-token-expired")
+    public void accessTokenExpiredException(){
+        throw new AccessTokenExpiredException();
+    }
+
+    @GetMapping("/invalid-token-exception")
+    public void invalidTokenException(){
+        throw new InvalidTokenException();
+    }
 }
