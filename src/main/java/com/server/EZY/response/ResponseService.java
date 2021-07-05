89aad1ac2dae1cb951dd547e731fa1b1d60b0fcc
siwony_ <siwon103305@gmail.com>
2021-07-05 16:43:32 +0900

    [UPDATE] responseService 주석 완료

--- src/main/java/com/server/EZY/response/ResponseService.java
+++ src/main/java/com/server/EZY/response/ResponseService.java
@@ -25,36 +25,66 @@ public class ResponseService {
         }
     }
 
-    public CommonResult getSuccessResult() {
-        return setSuccessResult(new CommonResult());
-    }
-
-    private CommonResult setSuccessResult(CommonResult result) {
-        result.setSuccess(true);
-        result.setCode(CommonResponse.SUCCESS.getCode());
-        result.setMassage(CommonResponse.SUCCESS.getMassage());
-
+    /**
+     * CommonResult setting method
+     * @param result
+     * @param commonResponse
+     * @return
+     * @author 정시원
+     */
+    private CommonResult setResponseResult(CommonResult result, CommonResponse commonResponse){
+        result.setSuccess(commonResponse == CommonResponse.SUCCESS ? true : false);
+        result.setCode(commonResponse.getCode());
+        result.setMassage(commonResponse.getMassage());
         return result;
     }
 
+    /**
+     * 요청 데이터가 없는 성공결과 객체 반환
+     * @return CommonResult - 성공결과 객체
+     * @author 정시원
+     */
+    public CommonResult getSuccessResult() {
+        return setResponseResult(new CommonResult(), CommonResponse.SUCCESS);
+    }
+
+    /**
+     * 단일건의 데이터가 있는 결과객체 반환
+     * @param data 단일 데이터
+     * @param <T> 단일 데이터 타입
+     * @return SingleResult - 단일건의 데이터가 있는 결과객체
+     * @author 정시원
+     */
     public <T> SingleResult<T> getSingleResult(T data){
         return new SingleResult<T>(getSuccessResult(), data);
     }
 
+
+    /**
+     * 다중값의 데이터가 있는 결과객체 반환
+     * @param list 다중 데이터
+     * @param <T> 다중 데이터 타입
+     * @return SingleResult - 다중값의 데이터가 있는 결과객체
+     * @author 정시원
+     */
     public <T> ListResult<T> getListResult(List<T> list){
         return new ListResult<T>(getSuccessResult(), list);
     }
 
-    // 실패 결과만 처리하는 메소드
+    /**
+     * 반환값이 없는 실패결과 객체 반환
+     * @return CommonResult 실패결과 객체
+     */
     public CommonResult getFailResult() {
-        CommonResult result = new CommonResult();
-        result.setSuccess(false);
-        result.setCode(CommonResponse.FAIL.getCode());
-        result.setMassage(CommonResponse.FAIL.getMassage());
-        return result;
+        return setResponseResult(new CommonResult(), CommonResponse.FAIL);
     }
 
-    //getFailResult 메소드가 code, msg를 받을수 있도록 수정
+    /**
+     * 실패결과 커스텀 세팅객체 반환
+     * @param code 반환할 code
+     * @param msg 반환할 message
+     * @return CommonResult - 실패결과 객체
+     */
     public CommonResult getFailResult(int code, String msg) {
         CommonResult result = new CommonResult();
         result.setSuccess(false);
