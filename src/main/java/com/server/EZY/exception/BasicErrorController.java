43fa1fdbcad2c1994840e57ac1f28edd33c660d5
siwony_ <siwon103305@gmail.com>
2021-07-07 08:47:15 +0900

    [ADD] basic error controller

---
+++ src/main/java/com/server/EZY/exception/BasicErrorController.java
@@ -0,0 +1,10 @@
+package com.server.EZY.exception;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class BasicErrorController {
+
+}
