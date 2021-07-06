72a4f0ffb10eaa943f330ea20bbc933759a5d554
siwony_ <siwon103305@gmail.com>
2021-07-06 10:40:03 +0900

    [UPDATE] DefaulteException 검증

--- src/test/java/com/server/EZY/exception/ExceptionAdviceTest.java
+++ src/test/java/com/server/EZY/exception/ExceptionAdviceTest.java
@@ -4,24 +4,26 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.EZY.exception.user.UserExceptionController;
 import com.server.EZY.exception.user.exception.UserNotFoundException;
+
 import com.server.EZY.response.result.CommonResult;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.tomcat.jni.Local;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -33,38 +35,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest @DisplayName("ExceptionAdvice test")
+@AutoConfigureMockMvc
 class ExceptionAdviceTest {
 
-    MockMvc mockMvc;
-    ResultActions resultActions;
+    @Autowired ObjectMapper objMapper;
+    @Autowired MessageSource messageSource;
     @Autowired ExceptionAdvice exceptionAdvice;
     @Autowired UserExceptionController userExceptionController;
-
-    @AfterEach
-    void resultActionsReset(){
-        resultActions = null;
-    }
 
     String objectToJson(Object object) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(object);
     }
+    // LocalContext의 locale을 변경한다.
+    void setLocal(Locale locale){
+        LocaleContextHolder.setLocale(locale);
+    }
+    // CommonResulte를 json 형식으로 반환한다.
+    String getResult(CommonResult commonResult) throws JsonProcessingException {
+        return objMapper.writeValueAsString(commonResult);
+    }
 
-    @Test @DisplayName("만약 UserNotFoundException이 발생한다면?")
-    void UserNotFoundException_검증() throws Exception {
+    void printResult(CommonResult commonResult_KO, CommonResult commonResult_EN) throws JsonProcessingException {
+        log.info("\n=== TEST result ===\n{}\n{}", getResult(commonResult_KO), getResult(commonResult_EN));
+    }
+
+    int getExceptionCode(String code, Locale locale){
+        return Integer.parseInt(messageSource.getMessage(code + ".code", null, locale));
+    }
+    String getExceptionMsg(String code, Locale locale){
+        return messageSource.getMessage(code + ".msg", null, locale);
+    }
+
+    @Test @DisplayName("UserNotFoundException 검증")
+    void DefaultException_검증() throws Exception {
         // Given
-        mockMvc = MockMvcBuilders.standaloneSetup(userExceptionController)
-                .setControllerAdvice(exceptionAdvice)
-                .addFilters(new CharacterEncodingFilter("UTF-8", true)) // utf-8 필터 추가
-                .build();
-        String userNotFoundExceptionResult = objectToJson(exceptionAdvice.userNotFoundException(new UserNotFoundException()));
+        setLocal(Locale.KOREA);
+        final int DEFAULT_EXCEPTION_CODE_KO = getExceptionCode(ExceptionAdvice.DEFAULT_EXCEPTION, Locale.KOREA);
+        final int USER_NOT_FOUND_EXCEPTION_CODE_EN = getExceptionCode(ExceptionAdvice.DEFAULT_EXCEPTION, Locale.ENGLISH);
+
+        final String USER_NOT_FOUND_EXCEPTION_MSG_KO = getExceptionMsg(ExceptionAdvice.DEFAULT_EXCEPTION, Locale.KOREA);
+        final String USER_NOT_FOUND_EXCEPTION_MSG_EN = getExceptionMsg(ExceptionAdvice.DEFAULT_EXCEPTION, Locale.ENGLISH);
 
         // When
-        resultActions = mockMvc.perform(get("/exception/user-not-found"));
+        CommonResult commonResult_KO = exceptionAdvice.defaultException(new Exception());
+        setLocal(Locale.ENGLISH);
+        CommonResult commonResult_EN = exceptionAdvice.defaultException(new Exception());
 
         // Then
-        resultActions
-                .andExpect(status().isNotFound());
-        log.info("=== UserNotFoundException Response ===\n{}", userNotFoundExceptionResult);
+        assertEquals(DEFAULT_EXCEPTION_CODE_KO, USER_NOT_FOUND_EXCEPTION_CODE_EN);
+
+        assertEquals(DEFAULT_EXCEPTION_CODE_KO, commonResult_KO.getCode());
+        assertEquals(USER_NOT_FOUND_EXCEPTION_CODE_EN, commonResult_EN.getCode());
+
+        assertEquals(USER_NOT_FOUND_EXCEPTION_MSG_KO, commonResult_KO.getMassage());
+        assertEquals(USER_NOT_FOUND_EXCEPTION_MSG_EN, commonResult_EN.getMassage());
+
+        printResult(commonResult_KO, commonResult_EN);
     }
+
 
 }
