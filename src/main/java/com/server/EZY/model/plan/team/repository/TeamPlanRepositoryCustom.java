97f07c901aabdf8bcffee695cc5782214fe5489e
johnjihwan <s20062@gsm.hs.kr>
2021-07-07 14:12:18 +0900

    Add findThisTeamPlanByUserEntity method

--- src/main/java/com/server/EZY/model/plan/team/repository/TeamPlanRepositoryCustom.java
+++ src/main/java/com/server/EZY/model/plan/team/repository/TeamPlanRepositoryCustom.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface TeamPlanRepositoryCustom {
     // UserEntity 를 param으로 넘겨주어 내 모든 TeamPlan을 조회하는 method
     List<HeadOfPlanEntity> findAllTeamPlanByUserEntity(UserEntity userEntity);
+    // UserEntity와 PlanIdx를 param으로 넘겨주어 해당 TeamPlan을 조회하는 method
+    HeadOfPlanEntity findThisTeamPlanByUserEntityAndHeadOfPlanIdx(UserEntity userEntity, Long planIdx);
 }
