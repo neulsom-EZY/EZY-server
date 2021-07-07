f92d47dd26a77009c4d0697a0d3131203f198964
johnjihwan <s20062@gsm.hs.kr>
2021-07-07 14:05:50 +0900

    Add description

--- src/main/java/com/server/EZY/model/plan/team/repository/TeamPlanRepositoryCustom.java
+++ src/main/java/com/server/EZY/model/plan/team/repository/TeamPlanRepositoryCustom.java
@@ -6,5 +6,6 @@ import com.server.EZY.model.user.UserEntity;
 import java.util.List;
 
 public interface TeamPlanRepositoryCustom {
+    // UserEntity 를 param으로 넘겨주어 내 모든 TeamPlan을 조회하는 method
     List<HeadOfPlanEntity> findAllTeamPlanByUserEntity(UserEntity userEntity);
 }
