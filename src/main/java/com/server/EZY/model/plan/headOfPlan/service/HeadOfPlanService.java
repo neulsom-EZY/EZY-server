a0eeb135c534405f763c1e572ddfe3183f0d2071
johnjihwan <s20062@gsm.hs.kr>
2021-07-08 12:16:16 +0900

    Add getAllPlan() method draft write

--- src/main/java/com/server/EZY/model/plan/headOfPlan/service/HeadOfPlanService.java
+++ src/main/java/com/server/EZY/model/plan/headOfPlan/service/HeadOfPlanService.java
@@ -1,2 +1,24 @@
-package com.server.EZY.model.plan.headOfPlan.service;public class HeadOfPlanService {
+package com.server.EZY.model.plan.headOfPlan.service;
+
+import com.server.EZY.model.plan.headOfPlan.HeadOfPlanEntity;
+import com.server.EZY.model.plan.headOfPlan.repository.HeadOfPlanRepository;
+import com.server.EZY.model.user.UserEntity;
+import com.server.EZY.util.CurrentUserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class HeadOfPlanService {
+    private final CurrentUserUtil currentUserUtil;
+    private final HeadOfPlanRepository headOfPlanRepository;
+
+    @Transactional
+    public List<HeadOfPlanEntity> getAllPlan() {
+        UserEntity getCurrentUser = currentUserUtil.getCurrentUser();
+        return headOfPlanRepository.findAllHeadOfPlanByUserEntity(getCurrentUser);
+    }
 }
