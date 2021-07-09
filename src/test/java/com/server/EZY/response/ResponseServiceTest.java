4c6051b4faf86c31cc8144d9fe67c20120b6422b
siwony_ <siwon103305@gmail.com>
2021-07-09 12:20:53 +0900

    [UPDSATE] CommonResponse 성공 message, code 검증

---
+++ src/test/java/com/server/EZY/response/ResponseServiceTest.java
@@ -0,0 +1,29 @@
+package com.server.EZY.response;
+
+import com.server.EZY.response.result.CommonResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResponseServiceTest {
+
+    @Autowired ResponseService responseService;
+
+    final String SUCCESS_MSG = ResponseService.CommonResponse.SUCCESS.massage;
+    final int SUCCESS_CODE = ResponseService.CommonResponse.SUCCESS.code;
+
+    final String FAIL_MSG = ResponseService.CommonResponse.FAIL.massage;
+    final int FAIL_CODE = ResponseService.CommonResponse.FAIL.code;
+
+    @Test @DisplayName("ResponseService.CommonResponse 성공시 message, code 검증")
+    void CommonResponse_성공_message_code_검증(){
+        assertEquals("성공하였습니다.", SUCCESS_MSG);
+        assertEquals(1, SUCCESS_CODE);
+    }
+
+
+}
