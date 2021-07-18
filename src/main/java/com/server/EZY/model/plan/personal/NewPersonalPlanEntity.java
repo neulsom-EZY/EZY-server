cd5bbc1fe1cc784be23438a2955ce1c7054f7228
siwony_ <siwon103305@gmail.com>
2021-07-18 12:20:10 +0900

    [ADD] ErrandStatus NewPersonalPlanEntity

--- src/main/java/com/server/EZY/model/plan/personal/NewPersonalPlanEntity.java
+++ src/main/java/com/server/EZY/model/plan/personal/NewPersonalPlanEntity.java
@@ -1,20 +1,26 @@
 package com.server.EZY.model.plan.personal;
 
+import com.server.EZY.model.plan.errand.ErrandStatus;
 import com.server.EZY.model.plan.headOfPlan.enumType.PlanDType;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Calendar;
 
+import static javax.persistence.FetchType.*;
+
 @Entity @Table(name = "NewPersonalPlan")
 @Builder @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class NewPersonalPlanEntity {
 
-    @Id
+    @Id @Column(name = "personal_plan_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "personal_plan_id")
     private Long personalPlanIdx;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "errand_status_id")
+    private ErrandStatus errandStatus;
 
     @Column(name = "title", nullable = false)
     private String title;
@@ -34,6 +40,6 @@ public class NewPersonalPlanEntity {
     private Boolean repetition;
 
     @Column(name = "d_type")
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private PlanDType DType;
 }
