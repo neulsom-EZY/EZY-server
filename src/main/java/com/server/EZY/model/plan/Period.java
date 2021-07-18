0b726fb4e8da3ec29530456d7400474113f89292
siwony_ <siwon103305@gmail.com>
2021-07-18 18:06:02 +0900

    [UPDATE] 주석 추가

--- src/main/java/com/server/EZY/model/plan/Period.java
+++ src/main/java/com/server/EZY/model/plan/Period.java
@@ -8,6 +8,10 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
 
+/**
+ * 일정(Plan)의 기간을 임베디드 타입으로 분리한 클래스
+ * @author 정시원
+ */
 @Embeddable
 @Builder @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
@@ -21,6 +25,11 @@ public class Period {
     @Temporal(TemporalType.TIMESTAMP)
     private Date endTime;
 
+    /**
+     * Period객체를 업데이트 한다.
+     * @param updatedPeriod 업데이트 할 Period타입의 인자
+     * @author 정시원
+     */
     public void updatePeriod(Period updatedPeriod){
         this.startTime = updatedPeriod.startTime != null ? updatedPeriod.startTime : this.startTime;
         this.endTime = updatedPeriod.endTime != null ? updatedPeriod.endTime : this.endTime;
--- src/main/java/com/server/EZY/model/plan/PlanInfo.java
+++ src/main/java/com/server/EZY/model/plan/PlanInfo.java
@@ -4,8 +4,11 @@ import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
 
+/**
+ * 일정(Plan)의 정보를 임베디드 타입으로 분리한 클래스
+ * @author 정시원
+ */
 @Embeddable
 @Builder @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
@@ -17,6 +20,11 @@ public class PlanInfo {
     @Column(name = "explanation", nullable = true)
     private String explanation;
 
+    /**
+     * PlanInfo를 업데이트 하는 매서드
+     * @param updatedPlanInfo 업데이트 할 PlanInfo타입의 인자
+     * @author 정시원
+     */
     public void updatePlanInfo(PlanInfo updatedPlanInfo){
         this.title = updatedPlanInfo.title != null ? updatedPlanInfo.title : this.title;
         this.explanation = updatedPlanInfo.explanation != null ? updatedPlanInfo.explanation : this.explanation;
