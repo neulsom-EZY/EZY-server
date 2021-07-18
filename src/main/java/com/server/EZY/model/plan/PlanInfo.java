1f56c7d20cb68f3dc9741ec6a918bc947ce4ad76
siwony_ <siwon103305@gmail.com>
2021-07-18 13:35:42 +0900

    [ADD] Entity 추상화를 위한 일정의 정보를 담고 있는 PlanInfo Embedd type의 객체 만듦

--- src/main/java/com/server/EZY/model/plan/PlanInfo.java
+++ src/main/java/com/server/EZY/model/plan/PlanInfo.java
@@ -1,2 +1,23 @@
-package com.server.EZY.model.plan;public class PlanInfo {
+package com.server.EZY.model.plan;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Embeddable
+@Builder @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class PlanInfo {
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "explanation", nullable = true)
+    private String explanation;
+
+    @Embedded
+    private Period period;
+
 }
