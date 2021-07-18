89b5ca931aca10dc958980bdec41d5ef63465b52
siwony_ <siwon103305@gmail.com>
2021-07-18 19:43:01 +0900

    [UPDATE] NewPersonalPlanEntity 심부름, 개인일정을 구분하는 생성자 만듦

--- src/main/java/com/server/EZY/model/plan/personal/NewPersonalPlanEntity.java
+++ src/main/java/com/server/EZY/model/plan/personal/NewPersonalPlanEntity.java
@@ -4,6 +4,7 @@ import com.server.EZY.model.plan.Period;
 import com.server.EZY.model.plan.PlanInfo;
 import com.server.EZY.model.plan.errand.ErrandStatus;
 import com.server.EZY.model.plan.headOfPlan.enumType.PlanDType;
+import com.server.EZY.model.user.UserEntity;
 import lombok.*;
 
 import javax.persistence.*;
@@ -12,12 +13,16 @@ import static javax.persistence.FetchType.*;
 
 @Entity @Table(name = "new_personal_plan")
 @Builder @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NewPersonalPlanEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "personal_plan_id")
     private Long personalPlanIdx;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "errand_status_id")
@@ -32,9 +37,60 @@ public class NewPersonalPlanEntity {
     @Column(name = "repetition")
     private Boolean repetition;
 
-    @Column(name = "d_type", nullable = true)
+    @Column(name = "d_type" )
     @Enumerated(EnumType.STRING)
     private PlanDType dType;
+
+    /**
+     * 개인일정을 추가하는 생성자
+     * @param userEntity 연관관계를 맻을 유저엔티티
+     * @param planInfo 개인일정의 기본적인 정보
+     * @param period 개인일정의 기간
+     * @param repetition 반복여부
+     * @author 정시원
+     */
+    public NewPersonalPlanEntity(UserEntity userEntity, PlanInfo planInfo, Period period, Boolean repetition){
+        this(userEntity, planInfo, period);
+        if(repetition != null){
+            this.repetition = repetition;
+            this.dType = PlanDType.PERSONAL_PLAN;
+        }else{
+            throw new IllegalArgumentException("null값이 들어갈 수 없습니다.");
+        }
+    }
+
+    /**
+     * 심부름을 추가하는 생성자
+     * @param userEntity 연관관계를 맻을 "유저 엔티티"
+     * @param planInfo 심부름의 정보를 담고 있는 임베디드 타입의 객체
+     * @param period 심부름의 기간 정보를 담고 있는 임베디드 타입의 객체
+     * @param errandStatus 심부름의 상태를 나타내는 "심부름상태 엔티티"
+     * @author 정시원
+     */
+    public NewPersonalPlanEntity(UserEntity userEntity ,PlanInfo planInfo, Period period, ErrandStatus errandStatus){
+        this(userEntity, planInfo, period);
+        if(errandStatus != null){
+            this.errandStatus = errandStatus;
+            this.dType = PlanDType.ERRAND;
+        }else
+            throw new IllegalArgumentException("null값이 들어갈 수 없습니다.");
+    }
+
+    /**
+     * 기본적인 정보를 통해 객체를 생성하는 생성자
+     * @param userEntity 연관관계를 맻을 "유저 엔티티"
+     * @param planInfo 일정의 기본적인 정보
+     * @param period 일정의 기간
+     * @author 정시원
+     */
+    private NewPersonalPlanEntity(UserEntity userEntity, PlanInfo planInfo, Period period){
+        if(userEntity != null && planInfo != null && period != null){
+            this.userEntity = userEntity;
+            this.planInfo = planInfo;
+            this.period = period;
+        }else
+            throw new IllegalArgumentException("null값이 들어갈 수 없습니다.");
+    }
 
     /**
      * personalPlan를 업데이트 하는 매서드
