c8ed0f75b75f9bfdd329699abab6dbdae213a76e
siwony_ <siwon103305@gmail.com>
2021-07-26 00:12:22 +0900

    [UPDATE] 생성자 빌더패턴 적용 및 update 매서드 완료

--- src/main/java/com/server/EZY/model/plan/personal/PersonalPlanEntity.java
+++ src/main/java/com/server/EZY/model/plan/personal/PersonalPlanEntity.java
@@ -1,5 +1,6 @@
 package com.server.EZY.model.plan.personal;
 
+import com.server.EZY.exception.user.exception.InvalidAccessException;
 import com.server.EZY.model.plan.PlanEntity;
 import com.server.EZY.model.plan.embeddedTypes.Period;
 import com.server.EZY.model.plan.embeddedTypes.PlanInfo;
@@ -28,6 +29,7 @@ public class PersonalPlanEntity extends PlanEntity {
      * @param repetition 반복여부
      * @author 정시원
      */
+    @Builder
     public PersonalPlanEntity(MemberEntity memberEntity, TagEntity tagEntity, PlanInfo planInfo, Period period, boolean repetition){
         super(memberEntity, tagEntity, planInfo, period);
         this.repetition = repetition;
@@ -37,22 +39,18 @@ public class PersonalPlanEntity extends PlanEntity {
      * personalPlan를 업데이트 하는 매서드
      * 현재 겍체에 저장되어있는 memberEntity와 업데이트를 하려는 currentMember를 동등성 비교를 해서 이 일정의 소유자일 경우에 이 객체를 변경할 수 있다.
      * @param currentMember 현제 로그인 한 유저
-     * @param tagEntity 변경할 테그
-     * @param planInfo 변경할 개인일정 정보
-     * @param period 변경할 개인일정 기간
-     * @param repetition 변경할 개인일정 반복
+     * @param personalPlanEntity 변경할 필드를 가지고 있는 PersonalPlanEntity 주로 DTO에서 toEntity 매서드로 생성된 PersonalPlanEntity를 인수로 넘겨받는다.
      * @author 정시원
      */
-    public void updatePersonalPlanEntity(MemberEntity currentMember, TagEntity tagEntity, PlanInfo planInfo, Period period, Boolean repetition) throws Exception {
+    public void updatePersonalPlanEntity(MemberEntity currentMember, PersonalPlanEntity personalPlanEntity) throws Exception {
         if (this.memberEntity.equals(currentMember)) {
-
-            this.tagEntity = tagEntity != null ? tagEntity : this.tagEntity;
-            this.planInfo = planInfo != null ? planInfo : this.planInfo;
-            this.period = period != null ? period : this.period;
-            this.repetition =  repetition != null ? repetition : this.repetition;
+            this.tagEntity = personalPlanEntity.tagEntity != null ? personalPlanEntity.tagEntity : this.tagEntity;
+            this.planInfo = personalPlanEntity.planInfo != null ? personalPlanEntity.planInfo : this.planInfo;
+            this.period = personalPlanEntity.period != null ? personalPlanEntity.period : this.period;
+            this.repetition =  personalPlanEntity.repetition != null ? personalPlanEntity.repetition : this.repetition;
         } else {
             log.debug("해당 개인일정을 다른 소유자가 변경할 수 없습니다..");
-            throw new Exception("해당 일정에 대한 접근권한이 없습니다."); // Exception 추가 예정
+            throw new InvalidAccessException();
         }
     }
 
