bd8fbdb219a014463a39e22e64f47d81f0c8ba4c
siwony_ <siwon103305@gmail.com>
2021-07-25 21:00:00 +0900

    [ADD] ErrandEntity

--- src/main/java/com/server/EZY/model/plan/errand/ErrandStatusEntity.java
+++ src/main/java/com/server/EZY/model/plan/errand/ErrandStatusEntity.java
@@ -1,5 +1,10 @@
 package com.server.EZY.model.plan.errand;
 
+import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.plan.embeddedTypes.Period;
+import com.server.EZY.model.plan.embeddedTypes.PlanInfo;
+import com.server.EZY.model.plan.errand.enumType.ResponseStatus;
+import com.server.EZY.model.plan.tag.TagEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +14,7 @@ import javax.persistence.*;
 @Entity @Table(name = "errand_status")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ErrandStatus {
+public class ErrandStatusEntity {
 
     @Id @Column(name = "errand_status_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +25,9 @@ public class ErrandStatus {
 
     @Column(name = "recipient_id")
     private Long recipientIdx;
+
+    @Column(name = "response_status")
+    @Enumerated(EnumType.STRING)
+    private ResponseStatus responseStatus;
 
 }
