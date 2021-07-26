bbd1f250f94bc38e44e7716079eb9aa74998482d
johnjihwan <s20062@gsm.hs.kr>
2021-07-26 18:26:07 +0900

    Add personalPlanCustom Repo to use querydsl strategy

---
+++ src/main/java/com/server/EZY/model/plan/personal/repository/PersonalPlanRepoImpl.java
@@ -0,0 +1,14 @@
+package com.server.EZY.model.plan.personal.repository;
+
+import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.plan.personal.PersonalPlanEntity;
+
+import java.util.List;
+
+public class PersonalPlanRepoCustomImpl implements PersonalPlanRepoCustom{
+
+    @Override
+    public List<PersonalPlanEntity> findAllPersonalPlan(MemberEntity memberEntity) {
+        return null;
+    }
+}
