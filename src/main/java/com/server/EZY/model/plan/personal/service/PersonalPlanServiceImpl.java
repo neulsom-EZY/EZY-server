8aa679a712cb02d567b5bcf1d126923e907f09d7
johnjihwan <s20062@gsm.hs.kr>
2021-07-26 16:40:52 +0900

    Update personalPlanSave service logic을 작성했습니다.

--- src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/personal/service/PersonalPlanServiceImpl.java
@@ -1,7 +1,11 @@
 package com.server.EZY.model.plan.personal.service;
 
+import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.plan.personal.PersonalPlanEntity;
 import com.server.EZY.model.plan.personal.dto.PersonalPlanSetDto;
+import com.server.EZY.model.plan.personal.repository.PersonalPlanRepository;
+import com.server.EZY.model.plan.tag.TagEntity;
+import com.server.EZY.model.plan.tag.repository.TagRepository;
 import com.server.EZY.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +14,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PersonalPlanServiceImpl implements PersonalPlanService{
 
-    private final CurrentUserUtil currentUserUtil;
+    private final CurrentUserUtil userUtil;
+    private final TagRepository tagRepository;
+    private final PersonalPlanRepository personalPlanRepository;
 
     @Override
     public PersonalPlanEntity createPersonalPlan(PersonalPlanSetDto personalPlan) {
-        return null;
+        /**
+         * currentUser: 로그인된 memberEntity
+         * tagEntity: 등록할 tag Entity
+         */
+        MemberEntity currentUser = userUtil.getCurrentUser();
+        TagEntity tagEntity = tagRepository.findByTagIdx(personalPlan.getTag());
+        // 저장요청
+        return personalPlanRepository.save(personalPlan.saveToEntity(currentUser, tagEntity));
     }
 }
