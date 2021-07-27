1d73a67f0862faceae5dbcffbd0b75de55615751
qoxogus <a01034367055@gmail.com>
2021-07-27 13:23:38 +0900

    [UPDATE] add errand status cycle comment

--- src/main/java/com/server/EZY/model/plan/errand/controller/ErrandStatusCycleController.java
+++ src/main/java/com/server/EZY/model/plan/errand/controller/ErrandStatusCycleController.java
@@ -12,26 +12,56 @@ public class ErrandStatusCycleController {
 
     private final ResponseService responseService;
 
+    /**
+     * 심부름을 받은이가 심부름을 확인(읽음)여부 Controller
+     * @param errandIdx
+     * @return
+     * @author 배태현
+     */
     @GetMapping("/check/{errandIdx}")
     public CommonResult readThisSchedule(@PathVariable("errandIdx") Long errandIdx) {
         return responseService.getSuccessResult();
     }
 
+    /**
+     * 심부름 수락 Controller
+     * @param errandIdx
+     * @return
+     * @author 배태현
+     */
     @PutMapping("/accept/{errandIdx}")
     public CommonResult acceptErrand(@PathVariable("errandIdx") Long errandIdx) {
         return responseService.getSuccessResult();
     }
 
+    /**
+     * 심부름 거절 Controller
+     * @param errandIdx
+     * @return
+     * @author 배태현
+     */
     @PutMapping("/reject/{errandIdx}")
     public CommonResult rejectErrand(@PathVariable("errandIdx") Long errandIdx) {
         return responseService.getSuccessResult();
     }
 
+    /**
+     * 심부름 완료 Controller
+     * @param errandIdx
+     * @return
+     * @author 배태현
+     */
     @PutMapping("/complete/{errandIdx}")
     public CommonResult completeErrand(@PathVariable("errandIdx") Long errandIdx) {
         return responseService.getSuccessResult();
     }
 
+    /**
+     * 심부름 실패 Controller
+     * @param errandIdx
+     * @return
+     * @author 배태현
+     */
     @PutMapping("/fail/{errandIdx}")
     public CommonResult failErrand(@PathVariable("errandIdx") Long errandIdx) {
         return responseService.getSuccessResult();
