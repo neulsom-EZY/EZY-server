ec0eb21d750da9bb2d1d48b9680063fcc965f112
qoxogus <a01034367055@gmail.com>
2021-07-27 12:15:06 +0900

    [ADD] basic errand function controller

---
+++ src/main/java/com/server/EZY/model/plan/errand/controller/ErrandController.java
@@ -0,0 +1,34 @@
+package com.server.EZY.model.plan.errand.controller;
+
+import com.server.EZY.response.ResponseService;
+import com.server.EZY.response.result.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/errand")
+@RequiredArgsConstructor
+public class ErrandController {
+
+    private final ResponseService responseService;
+
+    @GetMapping("/{username}")
+    public CommonResult getErrandAlarm(@PathVariable("username") String username) {
+        return responseService.getSuccessResult();
+    }
+
+    @GetMapping("/errandIdx")
+    public CommonResult thisErrandDetailSelect(@PathVariable("errandIdx") Long id) {
+        return responseService.getSuccessResult();
+    }
+
+    @PostMapping("/send")
+    public CommonResult sendErrand() {
+        return responseService.getSuccessResult();
+    }
+
+    @DeleteMapping("/recipient/{errandIdx}")
+    public CommonResult deleteByReceiver(@PathVariable("errandIdx") Long id) {
+        return responseService.getSuccessResult();
+    }
+}
