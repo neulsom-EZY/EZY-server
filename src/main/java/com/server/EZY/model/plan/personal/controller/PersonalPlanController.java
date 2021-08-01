f68f1622d3c827240bfc703e1294b89a19213c5f
johnjihwan <s20062@gsm.hs.kr>
2021-08-01 16:22:56 +0900

    Add 기존에 Mock 으로 반들었던 method에 apiParams 달았음

--- src/main/java/com/server/EZY/model/plan/personal/controller/PersonalPlanController.java
+++ src/main/java/com/server/EZY/model/plan/personal/controller/PersonalPlanController.java
@@ -4,6 +4,8 @@ import com.server.EZY.model.plan.personal.dto.PersonalPlanSetDto;
 import com.server.EZY.model.plan.personal.service.PersonalPlanService;
 import com.server.EZY.response.ResponseService;
 import com.server.EZY.response.result.CommonResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +17,20 @@ public class PersonalPlanController {
     private final ResponseService responseService;
 
     @PostMapping("/personal")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
     public CommonResult savePersonalPlan(@RequestBody PersonalPlanSetDto personalPlanSetDto) {
         personalPlanService.createPersonalPlan(personalPlanSetDto);
         return responseService.getSuccessResult();
     }
 
     @GetMapping("/personal")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
     public CommonResult getAllPersonalPlan(){
         return responseService.getListResult(personalPlanService.getAllPersonalPlan());
     }
