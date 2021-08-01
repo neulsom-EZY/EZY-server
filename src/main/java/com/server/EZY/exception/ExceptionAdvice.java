de2f467cfd830efd26cb0e2190a429cef0692fd7
siwony_ <siwon103305@gmail.com>
2021-08-01 21:31:26 +0900

    [UPDATE] ExceptionAdvice에 MemberException관련 헨들링 제거

--- src/main/java/com/server/EZY/exception/ExceptionAdvice.java
+++ src/main/java/com/server/EZY/exception/ExceptionAdvice.java
@@ -23,19 +23,8 @@ public interface ExceptionAdvice {
     String CUSTOM_403_FORBIDDEN = "forbidden";
     String CUSTOM_404_NOT_FOUND = "not-found";
 
-    String MEMBER_NOT_FOUND = "member-not-found";
-    String MEMBER_ALREADY_EXIST = "member-already-exist";
-    String INVALID_ACCESS = "invalid-access";
-
     String INVALID_AUTHENTICATION_NUMBER = "invalid-authentication-number";
-    String USERNAME_NOT_FOUND = "username-not-found";
     String AUTHENTICATION_NUMBER_TRANSFER_FAILED = "authentication-number-transfer-failed";
-
-    String AUTHORIZATION_HEADER_IS_EMPTY = "authorization-header-is-empty";
-    String REFRESH_TOKEN_HEADER_IS_EMPTY = "refresh-token-header-is-empty";
-    String ACCESS_TOKEN_EXPIRED = "access-token-expired";
-    String INVALID_TOKEN = "invalid-token";
-    String TOKEN_LOGGED_OUT = "token-logged-out";
 
 //    @ExceptionHandler(Exception.class)
 //    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -53,29 +42,6 @@ public interface ExceptionAdvice {
     @ExceptionHandler(CustomNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     CommonResult notFoundException(CustomNotFoundException ex);
-
-
-    /*** Member Exceptions 시작***/
-    // 회원를 찾을 수 없습니다.
-    @ExceptionHandler(MemberNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    CommonResult memberNotFoundException(MemberNotFoundException ex);
-
-    // 해당 회원은 이미 존재합니다.
-    @ExceptionHandler(MemberAlreadyExistException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    CommonResult memberAlreadyExistException(MemberAlreadyExistException ex);
-
-    // 해당 유저이름으로 맴버를 찾을 수 없습니다.
-    @ExceptionHandler(UsernameNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    CommonResult usernameNotFoundException(UsernameNotFoundException ex);
-
-    // 잘못된 접근입니다.
-    @ExceptionHandler(InvalidAccessException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    CommonResult invalidAccessException(InvalidAccessException ex);
-
 
     /*** Authentication Number Exception 시작 ***/
     // 인증번호가 올바르지 않습니다
--- src/main/java/com/server/EZY/exception/ExceptionAdviceImpl.java
+++ src/main/java/com/server/EZY/exception/ExceptionAdviceImpl.java
@@ -50,37 +50,6 @@ public class ExceptionAdviceImpl implements ExceptionAdvice{
         return exceptionResponseObjectUtil.getExceptionResponseObj(CUSTOM_404_NOT_FOUND);
     }
 
-    /*** UserException 시작 ***/
-    @Override
-    public CommonResult memberNotFoundException(MemberNotFoundException ex){
-        log.debug("=== User Not Found Exception 발생 ===");
-        return exceptionResponseObjectUtil.getExceptionResponseObj(MEMBER_NOT_FOUND);
-    }
-
-    @Override
-    public CommonResult memberAlreadyExistException(MemberAlreadyExistException ex) {
-        log.debug("=== Member Already Exist Exception 발생 ===");
-        return exceptionResponseObjectUtil.getExceptionResponseObj(MEMBER_ALREADY_EXIST);
-    }
-
-    @Override
-    public CommonResult usernameNotFoundException(UsernameNotFoundException ex) {
-        log.debug("=== Username Not Found Exception 발생 ===");
-        CommonResult usernameNotFoundExceptionResponseObj = exceptionResponseObjectUtil.getExceptionResponseObj(USERNAME_NOT_FOUND);
-        // UsernameNotFoundExceptiond에서 해당 username으로 회원을 찾지 못했을 경우 해당 username를 Exception message에 포함하는 로직
-        String insertUsernameInExceptionMassage =
-                usernameNotFoundExceptionResponseObj.getMassage().replaceAll(":username", "'" + ex.getMessage() + "'");
-        usernameNotFoundExceptionResponseObj.setMassage(insertUsernameInExceptionMassage);
-
-        return usernameNotFoundExceptionResponseObj;
-    }
-
-    @Override
-    public CommonResult invalidAccessException(InvalidAccessException ex) {
-        log.debug("=== InvalidAccessException 발생 ===");
-        return exceptionResponseObjectUtil.getExceptionResponseObj(INVALID_ACCESS);
-    }
-
     @Override
     public CommonResult invalidAuthenticationNumberException(InvalidAuthenticationNumberException ex) {
         log.debug("=== InvalidAuthenticationNumberException 발생 ===");
