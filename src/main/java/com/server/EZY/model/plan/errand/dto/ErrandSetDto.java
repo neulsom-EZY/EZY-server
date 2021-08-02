09fe4431a6e8e8b163c25f17eaa8768b102497ef
johnjihwan <s20062@gsm.hs.kr>
2021-08-02 15:06:33 +0900

    Won'tFIx 생성자 변경에 따라 잠시 중단

--- src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
+++ src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
@@ -1,6 +1,8 @@
 package com.server.EZY.model.plan.errand.dto;
 
+import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.plan.embeddedTypes.Period;
+import com.server.EZY.model.plan.errand.ErrandEntity;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
@@ -14,4 +16,14 @@ public class ErrandSetDto {
     private Period period;
     @NotNull
     private String recipient;
+
+//    public ErrandEntity saveToEntity(MemberEntity memberEntity){
+//        return ErrandEntity.builder()
+//                .memberEntity(memberEntity)
+//                .tagEntity()
+//                .planInfo()
+//                .period()
+//                .errandStatusEntity()
+//                .build();
+//    }
 }
