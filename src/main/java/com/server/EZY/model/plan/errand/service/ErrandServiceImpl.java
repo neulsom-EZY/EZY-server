ed1e8d643b047589343e2ba0c5bc8827e78f14f3
johnjihwan <s20062@gsm.hs.kr>
2021-08-02 17:10:12 +0900

    Add sendErrand method

--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
@@ -1,8 +1,12 @@
 package com.server.EZY.model.plan.errand.service;
 
 import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.member.repository.MemberRepository;
 import com.server.EZY.model.plan.errand.ErrandEntity;
+import com.server.EZY.model.plan.errand.ErrandStatusEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
+import com.server.EZY.model.plan.errand.enumType.ResponseStatus;
+import com.server.EZY.model.plan.errand.repository.ErrandRepository;
 import com.server.EZY.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +15,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ErrandServiceImpl implements ErrandService{
     private final CurrentUserUtil currentUserUtil;
+    private final MemberRepository memberRepository;
+    private final ErrandRepository errandRepository;
 
     @Override
     public ErrandEntity sendErrand(ErrandSetDto errandSetDto) {
         MemberEntity currentUser = currentUserUtil.getCurrentUser();
-        return null;
+
+        ErrandStatusEntity errandStatusEntity = ErrandStatusEntity.builder()
+                .senderIdx(currentUser.getMemberIdx())
+                .recipientIdx(memberRepository.findByUsername(errandSetDto.getRecipient()).getMemberIdx())
+                .responseStatus(ResponseStatus.NOT_READ)
+                .build();
+
+        return errandRepository.save(errandSetDto.saveToEntity(currentUser, errandStatusEntity));
     }
 }
