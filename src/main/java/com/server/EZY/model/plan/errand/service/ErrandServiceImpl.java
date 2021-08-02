89a5bf15e5a8e18ed20e506b1156e9f4b9c66029
johnjihwan <s20062@gsm.hs.kr>
2021-08-02 15:06:40 +0900

    Won'tFIx 생성자 변경에 따라 잠시 중단

--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
@@ -1,15 +1,20 @@
 package com.server.EZY.model.plan.errand.service;
 
+import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.plan.errand.ErrandEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
+import com.server.EZY.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ErrandServiceImpl implements ErrandService{
+    private final CurrentUserUtil currentUserUtil;
+
     @Override
     public ErrandEntity sendErrand(ErrandSetDto errandSetDto) {
+        MemberEntity currentUser = currentUserUtil.getCurrentUser();
         return null;
     }
 }
