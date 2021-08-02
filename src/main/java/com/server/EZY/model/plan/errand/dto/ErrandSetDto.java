2c08ca2c24e1d09758ef145554c3364e4db46a46
johnjihwan <s20062@gsm.hs.kr>
2021-08-02 14:42:47 +0900

    Add ErrandSetDto except location feild

--- src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
+++ src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
@@ -1,4 +1,17 @@
 package com.server.EZY.model.plan.errand.dto;
 
+import com.server.EZY.model.plan.embeddedTypes.Period;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class ErrandSetDto {
+    @NotNull
+    private String title;
+    @NotNull
+    private Period period;
+    @NotNull
+    private String recipient;
 }
