3e76bd579456850583410e771ea81984240f8e81
johnjihwan <s20062@gsm.hs.kr>
2021-08-02 16:50:49 +0900

    Update ErrandSetDto redesgn

--- src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
+++ src/main/java/com/server/EZY/model/plan/errand/dto/ErrandSetDto.java
@@ -2,7 +2,9 @@ package com.server.EZY.model.plan.errand.dto;
 
 import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.plan.embeddedTypes.Period;
+import com.server.EZY.model.plan.embeddedTypes.PlanInfo;
 import com.server.EZY.model.plan.errand.ErrandEntity;
+import com.server.EZY.model.plan.errand.ErrandStatusEntity;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
@@ -10,20 +12,24 @@ import javax.validation.constraints.NotNull;
 @Getter @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class ErrandSetDto {
+    // title, explanation
     @NotNull
-    private String title;
+    private PlanInfo planInfo;
+    // startTime, endTime
     @NotNull
     private Period period;
+    private String location;
     @NotNull
     private String recipient;
 
-//    public ErrandEntity saveToEntity(MemberEntity memberEntity){
-//        return ErrandEntity.builder()
-//                .memberEntity(memberEntity)
-//                .tagEntity()
-//                .planInfo()
-//                .period()
-//                .errandStatusEntity()
-//                .build();
-//    }
+    public ErrandEntity saveToEntity(MemberEntity memberEntity, ErrandStatusEntity errandStatusEntity){
+        return ErrandEntity.builder()
+                .memberEntity(memberEntity)
+                .tagEntity(null)
+                .planInfo(planInfo)
+                .period(period)
+                .errandStatusEntity(errandStatusEntity)
+                .location(location)
+                .build();
+    }
 }
