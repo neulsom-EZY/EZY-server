13563a7d36840d074493fb91335f963e3d839e26
johnjihwan <s20062@gsm.hs.kr>
2021-08-03 16:54:28 +0900

    Add startTime 기준으로 personal-plan-entities 를 가져오는 query-method 작성

--- src/main/java/com/server/EZY/model/plan/personal/repository/PersonalPlanRepoCustom.java
+++ src/main/java/com/server/EZY/model/plan/personal/repository/PersonalPlanRepoCustom.java
@@ -3,9 +3,11 @@ package com.server.EZY.model.plan.personal.repository;
 import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.plan.personal.PersonalPlanEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PersonalPlanRepoCustom {
     List<PersonalPlanEntity> findAllPersonalPlanByMemberEntity(MemberEntity memberEntity);
+    List<PersonalPlanEntity> findAllPersonalPlanEntitiesByMemberEntityAndPeriodStartTime(MemberEntity memberEntity, LocalDateTime startDateTime);
     PersonalPlanEntity findThisPersonalPlanByMemberEntityAndPlanIdx(MemberEntity memberEntity, Long planIdx);
 }
