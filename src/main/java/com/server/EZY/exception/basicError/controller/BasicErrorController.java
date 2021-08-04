d38df9bcbca36468f3b5a016ef8ec0bb4c2cce2f
siwony_ <siwon103305@gmail.com>
2021-08-04 13:03:57 +0900

    [UPDATE] BasicErrorHandler Basic error의 baseURL를 public final static으로 정의해 정보의 응집력을 높임

--- src/main/java/com/server/EZY/exception/basicError/controller/BasicErrorController.java
+++ src/main/java/com/server/EZY/exception/basicError/controller/BasicErrorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/error/")
+@RequestMapping(BasicErrorHandler.BASIC_ERROR_BASE_URL)
 @RequiredArgsConstructor
 public class BasicErrorController {
 
