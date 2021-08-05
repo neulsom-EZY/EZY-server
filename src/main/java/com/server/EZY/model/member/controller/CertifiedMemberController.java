10732605d6c2771d4bd8437fee010ca224f84c6a
qoxogus <a01034367055@gmail.com>
2021-08-06 02:25:10 +0900

    [ADD] change password controller

--- src/main/java/com/server/EZY/model/member/controller/CertifiedMemberController.java
+++ src/main/java/com/server/EZY/model/member/controller/CertifiedMemberController.java
@@ -1,6 +1,7 @@
 package com.server.EZY.model.member.controller;
 
 import com.server.EZY.model.member.dto.AuthDto;
+import com.server.EZY.model.member.dto.PhoneNumberChangeDto;
 import com.server.EZY.model.member.service.MemberService;
 import com.server.EZY.response.ResponseService;
 import com.server.EZY.response.result.CommonResult;
@@ -22,6 +23,17 @@ public class CertifiedMemberController {
     private final ResponseService responseService;
 
     //이 Controller가 return이 아무값도 return되지않음 공백이 뜸 (POSTMAN에서 확인)
+
+    @PutMapping("/change/phone")
+    @ResponseStatus( HttpStatus.OK )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
+    public CommonResult changePhoneNumber(@RequestBody PhoneNumberChangeDto phoneNumberChangeDto) {
+        memberService.changePhoneNumber(phoneNumberChangeDto);
+        return responseService.getSuccessResult();
+    }
 
     /**
      * "/v1/member/logout"로 요청이 들어오고 (로그인이 되어있는 상태)를 확인했기 때문에 logout한다...
