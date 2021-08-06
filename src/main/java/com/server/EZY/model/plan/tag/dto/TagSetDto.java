ec02bef3970190e11138a1fdd60b254d2091ba82
johnjihwan <s20062@gsm.hs.kr>
2021-08-06 17:51:30 +0900

    Add TagSetDto

---
+++ src/main/java/com/server/EZY/model/plan/tag/dto/TagSetDto.java
@@ -0,0 +1,8 @@
+package com.server.EZY.model.plan.tag.dto;
+
+import lombok.*;
+
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class TagSetDto {
+}
