4ef2fb85d3105712d36f1ed48668e23199c5cdcd
siwony_ <siwon103305@gmail.com>
2021-08-06 17:30:27 +0900

    [ADD] TeamEntity

---
+++ src/main/java/com/server/EZY/model/plan/team/TeamEntity.java
@@ -0,0 +1,30 @@
+package com.server.EZY.model.plan.team;
+
+import com.server.EZY.model.member.MemberEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity @Table(name = "team")
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class TeamEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_plan_idx", nullable = false)
+    private Long teamPlanIdx;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity memberEntity;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "plan_id")
+    private TeamPlanEntity teamPlanEntity;
+
+    @Column(name = "team_leader_idx", updatable = false)
+    private Long teamLeaderIdx;
+
+}
