886d520253ba441adcd5350772e7f797dbaf7f9c
johnjihwan <s20062@gsm.hs.kr>
2021-08-07 16:17:37 +0900

    Add tag save to Entity

--- src/main/java/com/server/EZY/model/plan/tag/dto/TagSetDto.java
+++ src/main/java/com/server/EZY/model/plan/tag/dto/TagSetDto.java
@@ -1,8 +1,25 @@
 package com.server.EZY.model.plan.tag.dto;
 
+import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.plan.tag.TagEntity;
+import com.server.EZY.model.plan.tag.embeddedTypes.Color;
 import lombok.*;
+
+import javax.validation.constraints.NotNull;
 
 @Getter @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class TagSetDto {
+    @NotNull
+    private Color color;
+    @NotNull
+    private String tag;
+
+    public TagEntity saveToEntity(MemberEntity memberEntity){
+        return TagEntity.builder()
+                .memberEntity(memberEntity)
+                .tag(this.tag)
+                .color(this.color)
+                .build();
+    }
 }
