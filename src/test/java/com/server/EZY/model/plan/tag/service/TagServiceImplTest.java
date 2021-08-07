a0c11222258e90b01623c497144a2d205663971f
johnjihwan <s20062@gsm.hs.kr>
2021-08-07 16:35:36 +0900

    Add TagService test success

--- src/test/java/com/server/EZY/model/plan/tag/service/TagServiceImplTest.java
+++ src/test/java/com/server/EZY/model/plan/tag/service/TagServiceImplTest.java
@@ -4,9 +4,13 @@ import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.member.dto.MemberDto;
 import com.server.EZY.model.member.enumType.Role;
 import com.server.EZY.model.member.repository.MemberRepository;
+import com.server.EZY.model.plan.tag.TagEntity;
+import com.server.EZY.model.plan.tag.dto.TagSetDto;
+import com.server.EZY.model.plan.tag.embeddedTypes.Color;
 import com.server.EZY.util.CurrentUserUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +18,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -21,9 +26,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+@Commit
 class TagServiceImplTest {
 
+    @Autowired
+    private TagService tagService;
     @Autowired
     private MemberRepository memberRepository;
     @Autowired
@@ -59,5 +66,22 @@ class TagServiceImplTest {
         assertEquals("배태현", currentUserNickname);
     }
 
+    @Test @DisplayName("Tag 를 저장할 수 있나요?")
+    void 태그저장(){
+        //Given
+        TagSetDto tagSetDto = TagSetDto.builder()
+                .tag("지환이 태그")
+                .color(Color.builder()
+                        .red((short) 196)
+                        .green((short) 200)
+                        .blue((short) 255)
+                        .build())
+                .build();
 
+        //When
+        TagEntity saveTag = tagService.saveTag(tagSetDto);
+
+        //Then
+        assertEquals(tagSetDto.getTag(), saveTag.getTag());
+    }
 }
