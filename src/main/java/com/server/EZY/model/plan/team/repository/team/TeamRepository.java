58e6ae8ae170ff4ca7c828e97872b03e80295019
siwony_ <siwon103305@gmail.com>
2021-08-07 21:54:29 +0900

    [FIX] JpaRepository<TeamPlanEntity, Long> -> JpaRepository<TeamEntity, Long>

--- src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
+++ src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
@@ -1,7 +1,7 @@
 package com.server.EZY.model.plan.team.repository.team;
 
-import com.server.EZY.model.plan.team.TeamPlanEntity;
+import com.server.EZY.model.plan.team.TeamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<TeamPlanEntity, Long> {
+public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
 }
