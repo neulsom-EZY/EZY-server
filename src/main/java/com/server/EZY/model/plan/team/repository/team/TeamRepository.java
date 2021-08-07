3d7c02b3092e5ffbb2541cdf20266062e1ddafd7
siwony_ <siwon103305@gmail.com>
2021-08-07 23:45:30 +0900

    [UPDATE] TeamPlanEntity로 TeamEntity를 모두 삭제하는 deleteAllByTeamPlanEntity추가

--- src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
+++ src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
     List<TeamEntity> findAllByTeamPlanEntity(TeamPlanEntity teamPlanEntity);
+    void deleteAllByTeamPlanEntity(TeamPlanEntity teamPlanEntity);ㅎ
 }
