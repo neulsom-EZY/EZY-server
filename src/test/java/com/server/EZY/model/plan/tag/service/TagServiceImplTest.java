b8fb593ed4d884a0939d5c2e6d9f5469bc9e77f8
johnjihwan <s20062@gsm.hs.kr>
2021-08-07 16:36:38 +0900

    Update @commit -> @Transactional

--- src/test/java/com/server/EZY/model/plan/tag/service/TagServiceImplTest.java
+++ src/test/java/com/server/EZY/model/plan/tag/service/TagServiceImplTest.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -26,7 +25,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Commit
+@Transactional
 class TagServiceImplTest {
 
     @Autowired
