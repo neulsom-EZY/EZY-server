e602df2afdf38c234d0fe087ec24f209fd91f96a
johnjihwan <s20062@gsm.hs.kr>
2021-08-07 16:40:43 +0900

    Update tag save controller

--- src/main/java/com/server/EZY/model/plan/tag/controller/TagController.java
+++ src/main/java/com/server/EZY/model/plan/tag/controller/TagController.java
@@ -1,5 +1,7 @@
 package com.server.EZY.model.plan.tag.controller;
 
+import com.server.EZY.model.plan.tag.dto.TagSetDto;
+import com.server.EZY.model.plan.tag.service.TagService;
 import com.server.EZY.response.ResponseService;
 import com.server.EZY.response.result.CommonResult;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TagController {
 
+    private final TagService tagService;
     private final ResponseService responseService;
 
     /**
      * 태그를 추가하는 Controller
-     * @param tag
-     * @return
-     * @author 배태현
+     * @param tagSetDto
+     * @return getSuccessResult
+     * @author 전지환
      */
     @PostMapping("")
-    public CommonResult addTag(String tag) {
+    public CommonResult addTag(@RequestBody TagSetDto tagSetDto) {
+        tagService.saveTag(tagSetDto);
         return responseService.getSuccessResult();
     }
 
