0f09eb543260336e2dff5b1b8500c2968895658c
johnjihwan <s20062@gsm.hs.kr>
2021-08-07 16:18:07 +0900

    Add TagService structure

--- src/main/java/com/server/EZY/model/plan/tag/service/TagServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/tag/service/TagServiceImpl.java
@@ -1,10 +1,18 @@
 package com.server.EZY.model.plan.tag.service;
 
+import com.server.EZY.model.plan.tag.TagEntity;
+import com.server.EZY.model.plan.tag.dto.TagSetDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TagServiceImpl implements TagService{
 
+    @Override
+    public TagEntity saveTag(TagSetDto tagSetDto) {
+        return null;
+    }
 }
