ce749bd4be283fff03c7d9bd4a0d57665593a770
siwony_ <siwon103305@gmail.com>
2021-08-07 22:59:20 +0900

    [UPDATE] TeamPlanEntity로 TeamEntity를 찾는 findAllByTeamPlanEntity 매서드 추가

--- src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
+++ src/main/java/com/server/EZY/model/plan/team/repository/team/TeamRepository.java
@@ -1,7 +1,11 @@
 package com.server.EZY.model.plan.team.repository.team;
 
 import com.server.EZY.model.plan.team.TeamEntity;
+import com.server.EZY.model.plan.team.TeamPlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
+    List<TeamEntity> findAllByTeamPlanEntity(TeamPlanEntity teamPlanEntity);
 }
