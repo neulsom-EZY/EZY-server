17add7c47176ffdb1fa387acbd302b4fe9c8b41e
siwony_ <siwon103305@gmail.com>
2021-08-08 18:51:11 +0900

    [UPDATE] Entity에 Min Max 추가

--- src/main/java/com/server/EZY/model/plan/tag/embeddedTypes/Color.java
+++ src/main/java/com/server/EZY/model/plan/tag/embeddedTypes/Color.java
@@ -5,18 +5,23 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import javax.validation.constraints.Size;
+
 @Embeddable
 @Builder @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class Color {
 
     @Column(name = "color_red", nullable = false, updatable = false)
+    @Size(min=0, max=255)
     private Short red;
 
     @Column(name = "color_green", nullable = false, updatable = false)
+    @Size(min=0, max=255)
     private Short green;
 
     @Column(name = "color_blue", nullable = false, updatable = false)
+    @Size(min=0, max=255)
     private Short blue;
 
 }
