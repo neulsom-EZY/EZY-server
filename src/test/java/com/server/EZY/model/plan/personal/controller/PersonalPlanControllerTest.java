0ff20522ba30296eb08b72b0a443db8b6644515d
johnjihwan <s20062@gsm.hs.kr>
2021-08-14 16:45:54 +0900

    Add personalPlanController basic body method

--- src/test/java/com/server/EZY/model/plan/personal/controller/PersonalPlanControllerTest.java
+++ src/test/java/com/server/EZY/model/plan/personal/controller/PersonalPlanControllerTest.java
@@ -1,4 +1,20 @@
+package com.server.EZY.model.plan.personal.controller;
+
+import com.server.EZY.testConfig.AbstractControllerTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
-class PersonalPlanControllerTest {
-  
+
+@SpringBootTest
+@Transactional
+class PersonalPlanControllerTest extends AbstractControllerTest {
+    @Autowired
+    private PersonalPlanController personalPlanController;
+
+    @Override
+    protected Object controller() {
+        return personalPlanController;
+    }
 }
