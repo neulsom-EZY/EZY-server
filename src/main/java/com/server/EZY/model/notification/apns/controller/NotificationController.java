570085545bce041cd35d92859afb67b36f088ba7
johnjihwan <s20062@gsm.hs.kr>
2021-08-24 17:11:40 +0900

    Add NotiController

--- src/main/java/com/server/EZY/model/notification/apns/controller/NotificationController.java
+++ src/main/java/com/server/EZY/model/notification/apns/controller/NotificationController.java
@@ -1,2 +1,10 @@
-package com.server.EZY.model.notification.apns.controller;public class NotificationController {
+package com.server.EZY.model.notification.apns.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/notification")
+public class NotificationController {
+
 }
