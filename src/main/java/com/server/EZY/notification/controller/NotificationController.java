423ed88ab9bc38907ede8a9316110ee1808a7fe2
johnjihwan <s20062@gsm.hs.kr>
2021-08-25 19:28:58 +0900

    Add initial class commit

--- src/main/java/com/server/EZY/notification/controller/NotificationController.java
+++ src/main/java/com/server/EZY/notification/controller/NotificationController.java
@@ -1,2 +1,14 @@
-package com.server.EZY.notification.controller;public class NotificationController {
+package com.server.EZY.notification.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/notification")
+@RequiredArgsConstructor
+public class NotificationController {
+
 }
