5555414aaa16262fdfddabe6e32c20ec653da839
johnjihwan <s20062@gsm.hs.kr>
2021-08-25 09:47:33 +0900

    Delete about APNs package, setting class

--- src/main/java/com/server/EZY/config/apns/ApnsConfig.java
+++
@@ -1,8 +0,0 @@
-package com.server.EZY.config.apns;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApnsConfig {
-    
-}
--- src/main/java/com/server/EZY/model/notification/apns/APNS.java
+++
@@ -1,5 +0,0 @@
-package com.server.EZY.model.notification.apns;
-
-public class APNS {
-    private APNS() {throw new AssertionError("Uninstantiable class");}
-}
