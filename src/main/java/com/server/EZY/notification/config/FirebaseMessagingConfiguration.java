4c4e30731c30605e337b7d9e71e9030e4aec76ce
johnjihwan <s20062@gsm.hs.kr>
2021-08-30 19:56:21 +0900

    Add FcmConfiguration

--- src/main/java/com/server/EZY/notification/config/FirebaseMessagingConfiguration.java
+++ src/main/java/com/server/EZY/notification/config/FirebaseMessagingConfiguration.java
@@ -1,4 +1,4 @@
-package com.server.EZY.notification.service;
+package com.server.EZY.notification.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
