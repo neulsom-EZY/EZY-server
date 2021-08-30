8eb9788a62d0bff02cc9cbda831d068f4b9b28af
johnjihwan <s20062@gsm.hs.kr>
2021-08-30 19:55:14 +0900

    Reset FirebaseMessagingService

---
+++ src/main/java/com/server/EZY/notification/config/FirebaseMessagingConfiguration.java
@@ -0,0 +1,23 @@
+package com.server.EZY.notification.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class FirebaseMessagingConfiguration {
+    @Bean
+    private FirebaseMessaging firebaseMessaging() throws IOException{
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build();
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "EZY");
+        return FirebaseMessaging.getInstance(app);
+    }
+}
--- src/main/java/com/server/EZY/notification/service/FirebaseMessagingService.java
+++ src/main/java/com/server/EZY/notification/service/FirebaseMessagingService.java
@@ -1,23 +1,10 @@
 package com.server.EZY.notification.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
+@Service
+@RequiredArgsConstructor
 public class FirebaseMessagingService {
-    @Bean
-    private FirebaseMessaging firebaseMessaging() throws IOException{
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
-        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
-                .build();
-        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "EZY");
-        return FirebaseMessaging.getInstance(app);
-    }
+
 }
