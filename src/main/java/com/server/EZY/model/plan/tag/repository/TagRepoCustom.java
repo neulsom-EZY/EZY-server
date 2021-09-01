0d2a7448acbdeb267a1ebcb4ceab38b346114a8c
johnjihwan <s20062@gsm.hs.kr>
2021-09-01 09:34:41 +0900

    Update return type tagGetDto

--- src/main/java/com/server/EZY/model/plan/tag/repository/TagRepoCustom.java
+++ src/main/java/com/server/EZY/model/plan/tag/repository/TagRepoCustom.java
@@ -1,10 +1,11 @@
 package com.server.EZY.model.plan.tag.repository;
 
 import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.plan.tag.dto.TagGetDto;
 import com.server.EZY.model.plan.tag.dto.TagSetDto;
 
 import java.util.List;
 
 public interface TagRepoCustom {
-    List<TagSetDto> findTagEntitiesByMemberEntity(MemberEntity memberEntity);
+    List<TagGetDto> findTagEntitiesByMemberEntity(MemberEntity memberEntity);
 }
--- src/main/java/com/server/EZY/model/plan/tag/repository/TagRepoImpl.java
+++ src/main/java/com/server/EZY/model/plan/tag/repository/TagRepoImpl.java
@@ -5,6 +5,7 @@ import static com.server.EZY.model.plan.tag.QTagEntity.tagEntity;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.plan.tag.dto.TagGetDto;
 import com.server.EZY.model.plan.tag.dto.TagSetDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +20,9 @@ public class TagRepoImpl implements TagRepoCustom {
 
     @Override
     @Transactional(readOnly = true)
-    public List<TagSetDto> findTagEntitiesByMemberEntity(MemberEntity memberEntity) {
+    public List<TagGetDto> findTagEntitiesByMemberEntity(MemberEntity memberEntity) {
         return queryFactory
-                .select(Projections.fields(TagSetDto.class,
+                .select(Projections.fields(TagGetDto.class,
                         tagEntity.tag,
                         tagEntity.color
                 ))
