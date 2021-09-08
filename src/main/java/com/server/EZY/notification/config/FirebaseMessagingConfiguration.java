67e776ffbf64ef2c00e03ecd437eeb87e2ad2884
johnjihwan <s20062@gsm.hs.kr>
2021-09-08 19:34:01 +0900

    Update 주석으로 설명문 작성

--- src/main/java/com/server/EZY/notification/config/FirebaseMessagingConfiguration.java
+++ src/main/java/com/server/EZY/notification/config/FirebaseMessagingConfiguration.java
@@ -11,6 +11,12 @@ public class FirebaseMessagingConfiguration {
     private static final String MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
     private static final String[] SCOPES = { MESSAGING_SCOPE };
 
+    /**
+     * 이 메서드는 google fcm 서버로 부터 Access token 을 발급 받기 위한 과정입니다.
+     * @return Access token.
+     * @throws IOException
+     * @author 전지환
+     */
     @Bean
     private static String getAccessToken() throws IOException {
         GoogleCredentials googleCredentials = GoogleCredentials
