589865fe924d0e2fde77ff22faa4d7720ef5763e
johnjihwan <s20062@gsm.hs.kr>
2021-09-09 15:07:55 +0900

    Add satisfied FcmRequest spec class

--- src/main/java/com/server/EZY/notification/FcmMessage.java
+++ src/main/java/com/server/EZY/notification/FcmMessage.java
@@ -2,11 +2,26 @@ package com.server.EZY.notification;
 
 import lombok.*;
 
-import java.util.Map;
-
-@Data
 public class FcmMessage {
-    private String subject;
-    private String content;
-    private Map<String, String> data;
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MessageResponse{
+        private String message;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FcmRequest{
+        private String token;
+        private String subject;
+        private String content;
+
+        public void setRequest(String subject, String content){
+            this.subject = subject;
+            this.content = content;
+        }
+    }
 }
