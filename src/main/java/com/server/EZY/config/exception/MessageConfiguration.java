c028c1f1d89a9ed8ae3472a3e37e5a4c52c614de
siwony_ <siwon103305@gmail.com>
2021-09-11 17:00:49 +0900

    Remark MessageConfiguration methods and inner class

--- src/main/java/com/server/EZY/config/exception/MessageConfiguration.java
+++ src/main/java/com/server/EZY/config/exception/MessageConfiguration.java
@@ -18,25 +18,46 @@ import java.util.ResourceBundle;
 @Configuration
 public class MessageConfiguration implements WebMvcConfigurer {
 
-    @Bean // 세션 지역설정
+    /**
+     * 기본적으로 세션을 한국으로 설정한다
+     * @return sessionLocaleResolver - 세션의 지역정보를 한국으로 설정한 객체
+     * @author 정시원
+     */
+    @Bean
     public SessionLocaleResolver sessionLocalResolver(){
         SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
         sessionLocaleResolver.setDefaultLocale(Locale.KOREAN);
         return sessionLocaleResolver;
     }
 
-    @Bean // 지역설정을 변경하는 인터셉터.
+    /**
+     * Request의 Locale 정보에 의해 Locale을 변경한 객체(LocaleChangeInterceptor)를 반환한다.
+     * @return LocaleChangeInterceptor
+     * @author 정시원
+     */
+    @Bean
     public LocaleChangeInterceptor localeChangeInterceptor(){
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
     }
 
-    @Override // 인터셉터를 시스템 레지스트리에 등록
+    /**
+     * 로컬 정보를 변경한 interceptor({@link #localeChangeInterceptor} 참고)를 시스템 레지스터리에 저장한다.
+     * 로컬 정보를 변경한 interceptor는 localeChangeInterceptor 매서드를 통해 생성한다.
+     * @param registry - InterceptorRegistry타입의 Interceptor의 저징 및 엑세스를 제공한다. (Spring에서 주입해준다.)
+     */
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
 
+    /**
+     * resources/i18n에 있는 메세지를 가져오기 위한 설정
+     * @param basename 사용자 지정 메시지가 정의되어있는 base디렉토리 위치를 나타낸다. (i18n/exception)
+     * @param encoding 인코딩 정보 (application.yml에 UTF-8로 설정했다.)
+     * @return yml메세지를 사용하기 위해 설정한 객체를 반환
+     */
     @Bean
     public MessageSource messageSource(
             @Value("${spring.messages.basename}") String basename,
@@ -51,8 +72,21 @@ public class MessageConfiguration implements WebMvcConfigurer {
         return ymlMessageSource;
     }
 
-    // locale 정보에 따라 다른 yml 파일을 읽도록 처리
+    /**
+     * 메세지 다국화를 위한 class<br>
+     * 해당 inner class는 다음 메서드에서 사용됩니다. {@link #messageSource(String, String)}
+     * @author 정시원
+     */
     private static class YamlMessageSource extends ResourceBundleMessageSource {
+        /**
+         * 파일이나 클래스의 정보를 가져오는 객체({@link ResourceBundle})를 반환하는 매서드 <br>
+         * 우리 프로젝트에서는 yml속 message를 가져오는데 사용한다.
+         * @param basename 사용자 지정 메시지가 정의되어있는 base디렉토리 위치를 나타낸다.
+         * @param locale 지역정보
+         * @return resourceBundle - java.utils에서 제공해주는 다국어 관련 객체
+         * @throws MissingResourceException 리소스가 없을 때 발생합니다.(예시. i18n속 yml에 메세지가 없을 때)
+         * @author 정시원
+         */
         @Override
         protected ResourceBundle doGetBundle(String basename, Locale locale) throws MissingResourceException {
             return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
