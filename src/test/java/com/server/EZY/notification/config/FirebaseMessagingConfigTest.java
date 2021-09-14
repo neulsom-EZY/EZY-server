33ace72c9cee02814b48d7b2f47a34b4742edc45
johnjihwan <s20062@gsm.hs.kr>
2021-09-14 09:43:18 +0900

    Update clean test class

--- src/test/java/com/server/EZY/notification/config/FirebaseMessagingConfigTest.java
+++ src/test/java/com/server/EZY/notification/config/FirebaseMessagingConfigTest.java
@@ -2,15 +2,10 @@ package com.server.EZY.notification.config;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class FirebaseMessagingConfigTest {
-
-    @Autowired
-    private FirebaseMessagingConfig firebaseMessagingConfig;
-
     @Test
     @DisplayName("token이 잘 가져와 지나요?")
     void getAccessToken(){
