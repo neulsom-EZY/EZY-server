c92cced1913dfd24cd450de0cf1354913048e8d9
johnjihwan <s20062@gsm.hs.kr>
2021-09-14 17:44:30 +0900

    Update sout -> log.info

--- src/test/java/com/server/EZY/notification/config/FirebaseMessagingConfigTest.java
+++ src/test/java/com/server/EZY/notification/config/FirebaseMessagingConfigTest.java
@@ -1,5 +1,6 @@
 package com.server.EZY.notification.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,11 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 
 @SpringBootTest
+@Slf4j
 public class FirebaseMessagingConfigTest {
     @Test
     @DisplayName("token이 잘 가져와 지나요?")
     public void getAccessToken() throws IOException {
         String accessToken = FirebaseMessagingConfig.getAccessToken();
-        System.out.println("accessToken = " + accessToken);
+        log.info("=========fcm AccessToken: {}===========", accessToken);
     }
 }
