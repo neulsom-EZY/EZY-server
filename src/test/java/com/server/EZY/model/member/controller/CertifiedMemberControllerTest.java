02b9a7f9e29eb1a36bba660b338607293b56f10b
qoxogus <a01034367055@gmail.com>
2021-09-28 18:17:25 +0900

    [ADD] annotation

--- src/test/java/com/server/EZY/model/member/controller/CertifiedMemberControllerTest.java
+++ src/test/java/com/server/EZY/model/member/controller/CertifiedMemberControllerTest.java
@@ -13,12 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Transactional
 public class CertifiedMemberControllerTest {
 
     @MockBean
