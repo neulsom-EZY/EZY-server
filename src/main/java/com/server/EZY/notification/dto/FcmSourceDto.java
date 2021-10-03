d29c7d52df8df33876bac145429a2d6a89d211b6
johnjihwan <s20062@gsm.hs.kr>
2021-10-03 19:24:35 +0900

    Add Fcm을 정확하게 보내는데 필요한 절차를 명시

--- src/main/java/com/server/EZY/notification/dto/FcmSourceDto.java
+++ src/main/java/com/server/EZY/notification/dto/FcmSourceDto.java
@@ -1,2 +1,20 @@
-package com.server.EZY.notification.dto;public class FcmSourceDto {
+package com.server.EZY.notification.dto;
+
+import com.server.EZY.notification.enum_type.FcmPurposeType;
+import com.server.EZY.notification.enum_type.FcmRole;
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FcmSourceDto {
+    @NotNull
+    private String sender;
+    @NotNull
+    private String recipient;
+    @NotNull
+    private FcmPurposeType fcmPurposeType;
+    @NotNull
+    private FcmRole fcmRole;
 }
