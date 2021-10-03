6dd12a18f8a7dc931b59a9979c0163809a8a38e9
johnjihwan <s20062@gsm.hs.kr>
2021-10-03 20:17:28 +0900

    Delete 쓸모가 없어진 레거시 메소드 삭제

--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
@@ -2,11 +2,7 @@ package com.server.EZY.model.plan.errand.service;
 
 import com.server.EZY.model.plan.errand.ErrandEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
-import com.server.EZY.model.plan.errand.enum_type.ErrandResponseStatus;
-import com.server.EZY.model.plan.errand.enum_type.ErrandRole;
-import com.server.EZY.notification.FcmMessage;
 
 public interface ErrandService {
     ErrandEntity sendErrand(ErrandSetDto errandSetDto) throws Exception;
-    FcmMessage.FcmRequest createFcmMessageAboutErrand(String sender, String recipient, ErrandRole errandRole, ErrandResponseStatus errandResponseStatus);
 }
--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
@@ -6,9 +6,7 @@ import com.server.EZY.model.plan.errand.ErrandEntity;
 import com.server.EZY.model.plan.errand.ErrandStatusEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
 import com.server.EZY.model.plan.errand.enum_type.ErrandResponseStatus;
-import com.server.EZY.model.plan.errand.enum_type.ErrandRole;
 import com.server.EZY.model.plan.errand.repository.ErrandRepository;
-import com.server.EZY.notification.FcmMessage;
 import com.server.EZY.notification.dto.FcmSourceDto;
 import com.server.EZY.notification.enum_type.FcmPurposeType;
 import com.server.EZY.notification.enum_type.FcmRole;
@@ -65,38 +63,5 @@ public class ErrandServiceImpl implements ErrandService{
         activeFcmFilterService.send(fcmSourceDto);
 
         return savedErrandEntity;
-    }
-
-    /**
-     * 심부름 관련 FcmMessage를 생성할 때 사용하는 메서드 입니다.
-     * @return FcmMessage.FcmRequest
-     * @author 전지환
-     */
-    @Override
-    public FcmMessage.FcmRequest createFcmMessageAboutErrand(String sender, String recipient, ErrandRole errandRole, ErrandResponseStatus errandResponseStatus){
-        String action="";
-
-        if (errandRole!=null){
-            switch (errandRole){
-                case SENDER: action = "요청";
-                    break;
-                case RECIPIENT: action = "전송";
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + errandRole);
-            }
-        } else if (errandResponseStatus!=null){
-            switch (errandResponseStatus){
-                case CANCEL: action = "거절";
-                    break;
-                case ACCEPT: action = "수락";
-                    break;
-            }
-        }
-
-        return FcmMessage.FcmRequest.builder()
-                .title("누군가 심부름을 " +action+"했어요")
-                .body(sender+" 님이 "+action+"한 심부름을 확인해보세요!")
-                .build();
     }
 }
