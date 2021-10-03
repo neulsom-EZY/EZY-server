55885524afb54588d9c9f016185f454535409bba
johnjihwan <s20062@gsm.hs.kr>
2021-10-03 19:25:11 +0900

    Fcm을 filter 하고 send까지 완료하는 로직을 작성

--- src/main/java/com/server/EZY/notification/service/ActiveFcmFilterService.java
+++ src/main/java/com/server/EZY/notification/service/ActiveFcmFilterService.java
@@ -1,5 +1,6 @@
 package com.server.EZY.notification.service;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.server.EZY.notification.dto.FcmSourceDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,19 +22,19 @@ public class ActiveFcmFilterService {
      * fcmSource를 받아 목적에 맞는 fcm 생성 로직으로 redirect 합니다.
      * @param fcmSourceDto
      */
-    public void checkFcmPurpose(FcmSourceDto fcmSourceDto){
+    public void checkFcmPurpose(FcmSourceDto fcmSourceDto) throws FirebaseMessagingException {
         log.info("===========FCM 사용 목적이 {}로 인식 됐습니다.", fcmSourceDto.getFcmPurposeType());
         switch (fcmSourceDto.getFcmPurposeType()){
-            case ERRAND: activeErrandFcm(fcmSourceDto);
+            case 심부름: activeErrandFcm(fcmSourceDto);
                 break;
-            case PERSONALPLAN:
+            case 개인일정:
                 break;
         }
     }
 
-    public void activeErrandFcm(FcmSourceDto fcmSourceDto){
+    public void activeErrandFcm(FcmSourceDto fcmSourceDto) throws FirebaseMessagingException {
         switch (fcmSourceDto.getFcmRole()){
-            case SENDER: fcmMakerService.errandSendFcm(fcmSourceDto);
+            case 보내는사람: fcmMakerService.errandSendFcm(fcmSourceDto);
         }
     }
 }
--- src/main/java/com/server/EZY/notification/service/FcmMakerService.java
+++ src/main/java/com/server/EZY/notification/service/FcmMakerService.java
@@ -1,13 +1,33 @@
 package com.server.EZY.notification.service;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.server.EZY.model.member.MemberEntity;
+import com.server.EZY.model.member.repository.MemberRepository;
 import com.server.EZY.notification.FcmMessage;
 import com.server.EZY.notification.dto.FcmSourceDto;
+import com.server.EZY.notification.enum_type.FcmActionSelector;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class FcmMakerService {
-    public void errandSendFcm(FcmSourceDto fcmSourceDto){
-        FcmMessage.FcmRequest.builder()
-                .title("누군가 "+fcmSourceDto.getFcmRole())
+    private final FirebaseMessagingService firebaseMessagingService;
+    private final MemberRepository memberRepository;
+
+    public void errandSendFcm(FcmSourceDto fcmSourceDto) throws FirebaseMessagingException {
+        FcmMessage.FcmRequest request = FcmMessage.FcmRequest.builder()
+                .title(
+                        "누군가 " + fcmSourceDto.getFcmPurposeType() + "을 " + FcmActionSelector.ErrandAction.요청 + " 했어요!"
+                )
+                .body(
+                        fcmSourceDto.getSender() + "님이 " + FcmActionSelector.ErrandAction.요청 + "한 심부름을 확인해보세요!"
+                ).build();
+
+        firebaseMessagingService.sendToToken(request, findRecipientFcmToken(fcmSourceDto.getRecipient()));
+    }
+
+    public String findRecipientFcmToken(String recipient){
+        return memberRepository.findByUsername(recipient).getFcmToken();
     }
 }
