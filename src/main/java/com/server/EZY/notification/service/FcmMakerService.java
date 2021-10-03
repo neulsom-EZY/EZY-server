17f9fcb9a73895f97b417de6c6e48f2766a9faa7
johnjihwan <s20062@gsm.hs.kr>
2021-10-03 20:00:03 +0900

    Refecroting..

--- src/main/java/com/server/EZY/notification/service/FcmMakerService.java
+++ src/main/java/com/server/EZY/notification/service/FcmMakerService.java
@@ -17,22 +17,15 @@ public class FcmMakerService {
     private final MemberRepository memberRepository;
 
     public void errandSendFcm(FcmSourceDto fcmSourceDto) throws FirebaseMessagingException {
-        log.info("==================정상적으로 심부름 fcm maker 메소드로 도착했습니다.===================");
         FcmMessage.FcmRequest request = FcmMessage.FcmRequest.builder()
-                .title(
-                        "누군가 " + fcmSourceDto.getFcmPurposeType() + "을 " + FcmActionSelector.ErrandAction.요청 + " 했어요!"
-                )
-                .body(
-                        fcmSourceDto.getSender() + "님이 " + FcmActionSelector.ErrandAction.요청 + "한 심부름을 확인해보세요!"
-                ).build();
-        log.info("==================정상적으로 심부름 fcm이 만들어졌습니다.===================");
+                .title("누군가 " + fcmSourceDto.getFcmPurposeType() + "을 " + FcmActionSelector.ErrandAction.요청 + " 했어요!")
+                .body(fcmSourceDto.getSender() + "님이 " + FcmActionSelector.ErrandAction.요청 + "한 심부름을 확인해보세요!")
+                .build();
+
         firebaseMessagingService.sendToToken(request, findRecipientFcmToken(fcmSourceDto.getRecipient()));
-        log.info("==================정상적으로 fcm 이 전송됐습니다.===================");
     }
 
     public String findRecipientFcmToken(String recipient){
-        String recipientFcmToken = memberRepository.findByUsername(recipient).getFcmToken();
-        log.info("==================정상적으로 받는사람의 fcmToken을 찾았습니다===================");
-        return recipientFcmToken;
+        return memberRepository.findByUsername(recipient).getFcmToken();
     }
 }
