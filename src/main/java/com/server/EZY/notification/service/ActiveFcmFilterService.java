7c4495a4574b3dae732defce638c51a5ae2847c3
johnjihwan <s20062@gsm.hs.kr>
2021-10-03 19:10:43 +0900

    Add 목적과 그에 맞는 fcmMaker메소드로 redirect 하는 로직 초안

--- src/main/java/com/server/EZY/notification/service/ActiveFcmFilterService.java
+++ src/main/java/com/server/EZY/notification/service/ActiveFcmFilterService.java
@@ -1,8 +1,8 @@
 package com.server.EZY.notification.service;
 
-import com.server.EZY.notification.FcmMessage;
 import com.server.EZY.notification.dto.FcmSourceDto;
-import com.server.EZY.notification.enum_type.FcmRole;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,18 +11,29 @@ import org.springframework.stereotype.Service;
  * @author 전지환
  */
 @Service
-public class ActiveFcmMessageService {
+@Slf4j
+@AllArgsConstructor
+public class ActiveFcmFilterService {
+
+    private final FcmMakerService fcmMakerService;
+
     /**
      * fcmSource를 받아 목적에 맞는 fcm 생성 로직으로 redirect 합니다.
      * @param fcmSourceDto
      */
     public void checkFcmPurpose(FcmSourceDto fcmSourceDto){
+        log.info("===========FCM 사용 목적이 {}로 인식 됐습니다.", fcmSourceDto.getFcmPurposeType());
         switch (fcmSourceDto.getFcmPurposeType()){
-            case ERRAND: activeErrandFcm(fcmSourceDto.getFcmRole()); break;
+            case ERRAND: activeErrandFcm(fcmSourceDto);
+                break;
+            case PERSONALPLAN:
+                break;
         }
     }
 
-    public FcmMessage.FcmRequest activeErrandFcm(FcmRole fcmRole){
-        return null;
+    public void activeErrandFcm(FcmSourceDto fcmSourceDto){
+        switch (fcmSourceDto.getFcmRole()){
+            case SENDER: fcmMakerService.errandSendFcm(fcmSourceDto);
+        }
     }
 }
--- src/main/java/com/server/EZY/notification/service/FcmMakerService.java
+++ src/main/java/com/server/EZY/notification/service/FcmMakerService.java
@@ -1,2 +1,13 @@
-package com.server.EZY.notification.service;public class FcmMakerService {
+package com.server.EZY.notification.service;
+
+import com.server.EZY.notification.FcmMessage;
+import com.server.EZY.notification.dto.FcmSourceDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FcmMakerService {
+    public void errandSendFcm(FcmSourceDto fcmSourceDto){
+        FcmMessage.FcmRequest.builder()
+                .title("누군가 "+fcmSourceDto.getFcmRole())
+    }
 }
