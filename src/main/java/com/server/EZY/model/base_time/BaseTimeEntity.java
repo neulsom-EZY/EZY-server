89a48864922b1f72077112bf85a08edd24c190fa
siwony_ <siwon103305@gmail.com>
2021-10-07 23:47:32 +0900

    Update BaseTimeEntity 추석 작성

--- src/main/java/com/server/EZY/model/base_time/BaseTimeEntity.java
+++ src/main/java/com/server/EZY/model/base_time/BaseTimeEntity.java
@@ -10,8 +10,15 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Getter
+/**
+ * 해당 Entity를 상속한 Entity는 컬럼 추가 시 시작시간, 종료시간 필드를 추가한다.
+ * @see com.server.EZY.model.plan.PlanEntity
+ * @author 정시원
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @MappedSuperclass @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseTimeEntity {
 
     @CreatedDate @Column(name = "create_date_time", updatable = false, nullable = false)
