417c098f7ea2f60bfedef3e7288fb516238cc01a
qoxogus <a01034367055@gmail.com>
2021-10-08 21:28:32 +0900

    [ADD] 인증번호 dto

--- src/main/java/com/server/EZY/model/member/dto/KeyDto.java
+++ src/main/java/com/server/EZY/model/member/dto/KeyDto.java
@@ -1,2 +1,17 @@
-package com.server.EZY.model.member.dto;public class KeyDto {
+package com.server.EZY.model.member.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+public class KeyDto {
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{4}$")
+    @Size(min = 4, max = 4)
+    private String key;
 }
