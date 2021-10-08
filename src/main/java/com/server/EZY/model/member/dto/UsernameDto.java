92d6e6b67d0f06ad8799c7dc4e18642b8036bf6c
qoxogus <a01034367055@gmail.com>
2021-10-08 10:31:44 +0900

    [UPDATE] Dto 제약조건 추가

--- src/main/java/com/server/EZY/model/member/dto/UsernameDto.java
+++ src/main/java/com/server/EZY/model/member/dto/UsernameDto.java
@@ -2,9 +2,16 @@ package com.server.EZY.model.member.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class UsernameDto {
 
+    @NotBlank
+    @Pattern(regexp = "^@[a-zA-Z]*$")
+    @Size(min = 1, max = 10)
     private String username;
 }
