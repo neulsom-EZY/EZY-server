cd8b88c0041c8e7cd26cfc1a95e537db9c1c5034
qoxogus <a01034367055@gmail.com>
2021-10-08 09:50:56 +0900

    [UPDATE] url 변경에 따른 접근 허용

--- src/main/java/com/server/EZY/security/SecurityConfig.java
+++ src/main/java/com/server/EZY/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v1/member/send/change/password/authkey").permitAll()//
                 .antMatchers("/v1/member/change/password").permitAll()//
                 .antMatchers("/v1/member/auth").permitAll()//
-                .antMatchers("/v1/member/auth/check").permitAll()//
+                .antMatchers("/v1/member/check/auth").permitAll()//
                 .antMatchers("/v1/member/refreshtoken").authenticated()// 로그인 된 유저는 모두 접근을 허용함
                 .antMatchers("/v1/member/logout").authenticated() // 로그인 된 유저는 모두 접근을 허용함
                 .antMatchers("/v1/member/delete").authenticated() // 로그인 된 유저는 모두 접근을 허용함
