8ac8ffb657c0616e75cf4d198fa0aa6cad504e8b
qoxogus <a01034367055@gmail.com>
2021-10-08 21:28:18 +0900

    [UPDATE] query param dto로 변경

--- src/main/java/com/server/EZY/model/member/controller/MemberController.java
+++ src/main/java/com/server/EZY/model/member/controller/MemberController.java
@@ -66,15 +66,15 @@ public class MemberController {
 
     /**
      * 전화번호로 인증번호를 전송하는 controller
-     * @param phoneNumber
+     * @param phoneNumberDto phoneNumber
      * @return CommonResult - SuccessResult
      * @author 배태현
      */
     @PostMapping("/auth")
     @ApiOperation(value = "전화번호로 인증번호 보내기", notes = "전화번호로 인증번호 보내기")
     @ResponseStatus( HttpStatus.OK )
-    public CommonResult sendAuthKey(String phoneNumber) {
-        memberService.sendAuthKey(phoneNumber);
+    public CommonResult sendAuthKey(@Valid @RequestBody PhoneNumberDto phoneNumberDto) {
+        memberService.sendAuthKey(phoneNumberDto.getPhoneNumber());
         return responseService.getSuccessResult();
     }
 
