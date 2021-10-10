6e395b99289121df8be5c205ccfc9ace9fb71ebb
siwony_ <siwon103305@gmail.com>
2021-10-11 05:09:33 +0900

    Update 사용자에게 Exception결과를 response하기위해 setResponseForExceptionResult만듦

--- src/main/java/com/server/EZY/security/exception_hendler/FilterExceptionHandlerFilter.java
+++ src/main/java/com/server/EZY/security/exception_hendler/FilterExceptionHandlerFilter.java
@@ -3,11 +3,14 @@ package com.server.EZY.security.exception_hendler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.EZY.exception.token.TokenExceptionHandler;
-import com.server.EZY.exception.token.TokenExceptionHandlerImpl;
 import com.server.EZY.exception.unknown_exception.UnknownExceptionHandler;
 import com.server.EZY.response.result.CommonResult;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,13 +35,30 @@ public class FilterExceptionHandlerFilter extends OncePerRequestFilter {
     private final UnknownExceptionHandler unknownExceptionHandler;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
         try{
             filterChain.doFilter(request, response);
+        } catch(ExpiredJwtException e) {
+            //TODO 만료된 Jwt Token에 대한 Exception결과 반환
+        } catch(JwtException | IllegalArgumentException e) {
+           //TODO 잘못된 Jwt Token에 대한 Exception결과 반환
         } catch(Exception e){
-            log.debug("=== Filter에서 알 수 없는 Exception이 발생했습니다. ===\n원인: {}", e.getMessage());
-            unknownExceptionHandler.defaultException(e);
+            log.error("=== Filter에서 알 수 없는 Exception이 발생했습니다. ===");
+            setResponseForExceptionResult(response, HttpStatus.INTERNAL_SERVER_ERROR, unknownExceptionHandler.defaultException(e));
         }
+    }
+
+    /**
+     * Exception결과를 json타입으로 response객체에 씁니다.
+     * @param response response
+     * @param httpStatus 사용자에게 반환할 HttpStatusCode
+     * @param commonResult 사용자에게 반환할 Exception결과를 가지고 있는 객체
+     * @throws IOException HttpServletResponse에 writer를 통해 body를 작성하면 발생할 수 있다.
+     */
+    private void setResponseForExceptionResult(HttpServletResponse response, HttpStatus httpStatus, CommonResult commonResult) throws IOException {
+        response.setStatus(httpStatus.value());
+        response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        response.getWriter().write(commonResultToJson(commonResult));
     }
 
     /**
