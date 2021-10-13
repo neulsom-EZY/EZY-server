2cb579deb5af5ec0d0b642f74de41378a774ce18
qoxogus <a01034367055@gmail.com>
2021-10-13 14:12:43 +0900

    [UPDATE] API명세서와 맞게 정렬, 로그인 후 이용할 수 있는 서비스 authenticated처리

--- src/main/java/com/server/EZY/security/SecurityConfig.java
+++ src/main/java/com/server/EZY/security/SecurityConfig.java
@@ -34,28 +34,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Entry points
-        http.authorizeRequests()//
+        http.authorizeRequests()
                 .antMatchers("/v1/member/verified/username").permitAll()
-                .antMatchers("/v1/member/signin").permitAll()//
-                .antMatchers("/v1/member/signup").permitAll()//
-                .antMatchers("/v1/member/find/username").permitAll()//
-                .antMatchers("/v1/member/change/username").authenticated()//
-                .antMatchers("/v1/member/find/username").permitAll()//
-                .antMatchers("/v1/member/send/change/password/authkey").permitAll()//
-                .antMatchers("/v1/member/change/password").permitAll()//
-                .antMatchers("/v1/member/auth").permitAll()//
-                .antMatchers("/v1/member/verified/auth").permitAll()//
-                .antMatchers("/v1/member/refreshtoken").authenticated()// 로그인 된 유저는 모두 접근을 허용함
-                .antMatchers("/v1/member/logout").authenticated() // 로그인 된 유저는 모두 접근을 허용함
-                .antMatchers("/v1/member/delete").authenticated() // 로그인 된 유저는 모두 접근을 허용함
-                .antMatchers("/v1/member/fcmtoken").authenticated()
+                .antMatchers("/v1/member/signup").permitAll()
+                .antMatchers("/v1/member/signin").permitAll()
+                .antMatchers("/v1/member/find/username").permitAll()
+                .antMatchers("/v1/member/auth").permitAll()
+                .antMatchers("/v1/member/verified/auth").permitAll()
+                .antMatchers("/v1/member/send/change/password/authkey").permitAll()
+                .antMatchers("/v1/member/change/password").permitAll()
+
                 /* 이렇게 권한에 따라 url접속을 제한할 수 있다. (테스트 완료)
                 .antMatchers("/v1/member/test").hasRole("CLIENT")
                 .antMatchers("/v1/admin/test").hasRole("ADMIN")
                 */
 
                 .antMatchers("/v1/errand/**").permitAll() //개발 편의상 permitAll 처리 해 두었음
-
                 .antMatchers("/v1/tag/**").permitAll() //개발 편의상 permitAll 처리 해 두었음
 
                 .antMatchers("/exception/**").permitAll()
