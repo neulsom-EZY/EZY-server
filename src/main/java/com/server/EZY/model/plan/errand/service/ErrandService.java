67afc197a3b6beb73953202c9b6d8d440ae9ffb4
siwony_ <siwon103305@gmail.com>
2021-10-13 11:18:23 +0900

    Update errandAccept interface에 정의

--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
@@ -1,8 +1,21 @@
 package com.server.EZY.model.plan.errand.service;
 
+import com.server.EZY.exception.response.CustomException;
+import com.server.EZY.exception.user.exception.InvalidAccessException;
 import com.server.EZY.model.plan.errand.ErrandEntity;
+import com.server.EZY.model.plan.errand.ErrandStatusEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
 
 public interface ErrandService {
     ErrandEntity sendErrand(ErrandSetDto errandSetDto) throws Exception;
+
+    /**
+     * 심부름을 수락한다. <br>
+     * 수신자의 ErrandStatus가 DB에 저장되고, 심부름을 수락했다는 push알람을 발신자에게 전송한다.
+     * @param errandIdx 수락할 errandIdx(planIdx)
+     * @return 수신자의 errandStatus
+     * @throws InvalidAccessException 해당 심부름에 잘못된 접근을 할 경우
+     * @throws CustomException PlanNotFound 해당 심부름이 존재하지 않을 때
+     */
+    ErrandStatusEntity acceptErrand(long errandIdx) throws InvalidAccessException, CustomException;
 }
--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandServiceImpl.java
@@ -1,5 +1,7 @@
 package com.server.EZY.model.plan.errand.service;
 
+import com.server.EZY.exception.response.CustomException;
+import com.server.EZY.exception.user.exception.InvalidAccessException;
 import com.server.EZY.model.member.MemberEntity;
 import com.server.EZY.model.member.repository.MemberRepository;
 import com.server.EZY.model.plan.errand.ErrandEntity;
@@ -63,5 +65,18 @@ public class ErrandServiceImpl implements ErrandService{
         activeFcmFilterService.send(fcmSourceDto);
 
         return savedErrandEntity;
+    }
+
+    /**
+     * 심부름을 수락한다. <br>
+     * 수신자의 ErrandStatus가 DB에 저장되고, 심부름을 수락했다는 push알람을 발신자에게 전송한다.
+     * @param errandIdx 수락할 errandIdx(planIdx)
+     * @return 수신자의 errandStatus
+     * @throws InvalidAccessException 해당 심부름에 잘못된 접근을 할 경우
+     * @throws CustomException        PlanNotFound 해당 심부름이 존재하지 않을 때
+     */
+    @Override
+    public ErrandStatusEntity acceptErrand(long errandIdx) {
+        return null;
     }
 }
