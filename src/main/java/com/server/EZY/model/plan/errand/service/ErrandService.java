38ce7add0de04abd491c5cdc63310bc13fe4783d
siwony_ <siwon103305@gmail.com>
2021-10-14 10:11:42 +0900

    Update errandAccept에서 수신자 심부름 정보 저장완료 앞으로 push알람 정의 예정

--- src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
+++ src/main/java/com/server/EZY/model/plan/errand/service/ErrandService.java
@@ -3,7 +3,6 @@ package com.server.EZY.model.plan.errand.service;
 import com.server.EZY.exception.response.CustomException;
 import com.server.EZY.exception.user.exception.InvalidAccessException;
 import com.server.EZY.model.plan.errand.ErrandEntity;
-import com.server.EZY.model.plan.errand.ErrandStatusEntity;
 import com.server.EZY.model.plan.errand.dto.ErrandSetDto;
 
 public interface ErrandService {
@@ -11,11 +10,11 @@ public interface ErrandService {
 
     /**
      * 심부름을 수락한다. <br>
-     * 수신자의 ErrandStatus가 DB에 저장되고, 심부름을 수락했다는 push알람을 발신자에게 전송한다.
+     * 수신자의 Errand가 DB에 저장되고, 심부름을 수락했다는 push알람을 발신자에게 전송한다.
      * @param errandIdx 수락할 errandIdx(planIdx)
-     * @return 수신자의 errandStatus
+     * @return 수신자의 ErrandEntity
      * @throws InvalidAccessException 해당 심부름에 잘못된 접근을 할 경우
-     * @throws CustomException PlanNotFound 해당 심부름이 존재하지 않을 때
+     * @throws CustomException        PlanNotFound 해당 심부름이 존재하지 않을 때
      */
-    ErrandStatusEntity acceptErrand(long errandIdx) throws InvalidAccessException, CustomException;
+    ErrandEntity acceptErrand(long errandIdx) throws InvalidAccessException, CustomException;
 }
