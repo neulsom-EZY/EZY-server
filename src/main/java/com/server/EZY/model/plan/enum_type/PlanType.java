196e595670da43270c45ecd8ce29fb13f0960bbf
siwony_ <siwon103305@gmail.com>
2021-10-18 16:45:34 +0900

    Add Plan에 대한 type을 정의한 PlanType 열거형 생성

---
+++ src/main/java/com/server/EZY/model/plan/enum_type/PlanType.java
@@ -0,0 +1,12 @@
+package com.server.EZY.model.plan.enum_type;
+
+/**
+ * 일정에 대한 타입들을 열거해놓은 enum class <br>
+ *
+ * @author 정시원
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public enum PlanType {
+    개인일정, 심부름, 팀일정
+}
