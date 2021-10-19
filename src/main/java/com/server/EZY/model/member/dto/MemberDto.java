8917f64e52bff01b5a526827ef52abd7964b0c62
qoxogus <a01034367055@gmail.com>
2021-10-19 15:54:35 +0900

    [UPDATE] 불변객체로 지정

--- src/main/java/com/server/EZY/model/member/dto/MemberDto.java
+++ src/main/java/com/server/EZY/model/member/dto/MemberDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import java.util.Collections;
 
 @Getter @Setter @Builder
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
 public class MemberDto {
     @NotBlank
     @Pattern(regexp = "^@[a-zA-Z]*$")
