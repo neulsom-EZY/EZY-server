50d2fe08c42224da80115d6f21669773c585520d
siwony_ <siwon103305@gmail.com>
2021-10-19 17:42:15 +0900

    Update ErrandStatusCycleController에 정의 되어있는 심부름 수락/삭제 method를 이용함

--- src/main/java/com/server/EZY/model/plan/errand/controller/ErrandStatusCycleController.java
+++ src/main/java/com/server/EZY/model/plan/errand/controller/ErrandStatusCycleController.java
@@ -1,7 +1,11 @@
 package com.server.EZY.model.plan.errand.controller;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.server.EZY.model.plan.errand.service.ErrandService;
 import com.server.EZY.response.ResponseService;
 import com.server.EZY.response.result.CommonResult;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class ErrandStatusCycleController {
 
     private final ResponseService responseService;
+    private final ErrandService errandService;
 
     /**
      * 심부름을 받은이가 심부름을 확인(읽음)여부 Controller
@@ -27,10 +32,14 @@ public class ErrandStatusCycleController {
      * 심부름 수락 Controller
      * @param errandIdx
      * @return
-     * @author 배태현
+     * @author 배태현, 정시원
      */
     @PutMapping("/accept/{errandIdx}")
-    public CommonResult acceptErrand(@PathVariable("errandIdx") Long errandIdx) {
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+    })
+    public CommonResult acceptErrand(@PathVariable("errandIdx") long errandIdx) throws FirebaseMessagingException {
+        errandService.acceptErrand(errandIdx);
         return responseService.getSuccessResult();
     }
 
@@ -38,10 +47,14 @@ public class ErrandStatusCycleController {
      * 심부름 거절 Controller
      * @param errandIdx
      * @return
-     * @author 배태현
+     * @author 배태현, 정시원
      */
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+    })
     @PutMapping("/reject/{errandIdx}")
-    public CommonResult rejectErrand(@PathVariable("errandIdx") Long errandIdx) {
+    public CommonResult rejectErrand(@PathVariable("errandIdx") long errandIdx) throws FirebaseMessagingException {
+        errandService.refuseErrand(errandIdx);
         return responseService.getSuccessResult();
     }
 
