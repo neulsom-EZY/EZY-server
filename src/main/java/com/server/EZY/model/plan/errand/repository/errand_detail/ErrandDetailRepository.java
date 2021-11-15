4d40d678905176108b7c1556f739ca078854b1e6
siwony_ <siwon103305@gmail.com>
2021-11-15 13:10:03 +0900

    Rename ErrandStatusRepository -> ErrandDetailRepository

--- src/main/java/com/server/EZY/model/plan/errand/repository/errand_detail/ErrandDetailRepository.java
+++ src/main/java/com/server/EZY/model/plan/errand/repository/errand_detail/ErrandDetailRepository.java
@@ -1,9 +1,9 @@
-package com.server.EZY.model.plan.errand.repository.errand_status;
+package com.server.EZY.model.plan.errand.repository.errand_detail;
 
 import com.server.EZY.model.plan.errand.ErrandDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ErrandStatusRepository extends JpaRepository<ErrandDetailEntity, Long> {
+public interface ErrandDetailRepository extends JpaRepository<ErrandDetailEntity, Long> {
 }
