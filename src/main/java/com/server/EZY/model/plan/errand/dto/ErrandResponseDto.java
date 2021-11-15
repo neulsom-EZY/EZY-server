eedcdf5659c0afc4ecfedd1a791c76e0b141759e
johnjihwan <s20062@gsm.hs.kr>
2021-11-15 11:40:13 +0900

    Add EZY api spec 에 맞춰 수정

--- src/main/java/com/server/EZY/model/plan/errand/dto/ErrandResponseDto.java
+++ src/main/java/com/server/EZY/model/plan/errand/dto/ErrandResponseDto.java
@@ -1,10 +1,10 @@
 package com.server.EZY.model.plan.errand.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
+import com.server.EZY.model.plan.embedded_type.Period;
+import com.server.EZY.model.plan.embedded_type.PlanInfo;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 public class ErrandResponseDto {
     /**
@@ -18,16 +18,14 @@ public class ErrandResponseDto {
         private Long planIdx;
         private String subject;
         private String title;
-        private LocalDateTime timeToStart;
-        private LocalDateTime timeToEnd;
+        private Period period;
 
         @QueryProjection
-        public Errands(Long planIdx, String subject, String title, LocalDateTime timeToStart, LocalDateTime timeToEnd) {
+        public Errands(Long planIdx, String subject, String title, Period period) {
             this.planIdx = planIdx;
             this.subject = subject;
             this.title = title;
-            this.timeToStart = timeToStart;
-            this.timeToEnd = timeToEnd;
+            this.period = period;
         }
     }
 }
