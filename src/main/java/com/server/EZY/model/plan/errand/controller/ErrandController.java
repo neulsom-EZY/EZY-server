fc6b1c046ae8454d08b1976f8946d1df76553d65
johnjihwan <s20062@gsm.hs.kr>
2021-11-20 14:47:59 +0900

    Update controller api authorization param

--- src/main/java/com/server/EZY/model/plan/errand/controller/ErrandController.java
+++ src/main/java/com/server/EZY/model/plan/errand/controller/ErrandController.java
@@ -44,6 +44,10 @@ public class ErrandController {
      * @author 전지환, 배태현
      */
     @GetMapping("/{errandIdx}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
+            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    })
     public CommonResult getThisErrandDetails(@PathVariable(value = "errandIdx") Long errandIdx) {
         return responseService.getSingleResult(errandService.findErrandDetails(errandIdx));
     }
