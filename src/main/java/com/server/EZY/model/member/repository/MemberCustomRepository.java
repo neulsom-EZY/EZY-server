1f5adc4e764f686c53feb5a2e16366ef31c54647
johnjihwan <s20062@gsm.hs.kr>
2021-11-27 17:55:31 +0900

    Add search username keyword based query method

---
+++ src/main/java/com/server/EZY/model/member/repository/MemberCustomRepository.java
@@ -0,0 +1,22 @@
+package com.server.EZY.model.member.repository;
+
+import com.server.EZY.model.member.dto.UsernameResponseDto;
+
+import java.util.List;
+
+/**
+ * member querydsl method
+ *
+ * @since 1.0.0
+ * @author 전지환
+ */
+public interface MemberCustomRepository {
+    /**
+     * 키워드 기반으로 username을 찾아주는 쿼리 메소드.
+     *
+     * @param keyword
+     * @return List<UsernameResponseDto>
+     * @author 전지환
+     */
+    List<UsernameResponseDto> searchUsernameKeywordBased(String keyword);
+}
---
+++ src/main/java/com/server/EZY/model/member/repository/MemberCustomRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.server.EZY.model.member.repository;
+
+import com.querydsl.jpa.JPQLQueryFactory;
+import com.server.EZY.model.member.dto.QUsernameResponseDto;
+import com.server.EZY.model.member.dto.UsernameResponseDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.server.EZY.model.member.QMemberEntity.memberEntity;
+
+@RequiredArgsConstructor
+public class MemberCustomRepositoryImpl implements MemberCustomRepository{
+
+    private final JPQLQueryFactory queryFactory;
+
+    /**
+     * 키워드 기반으로 username을 찾아주는 쿼리 메소드.
+     *
+     * @param keyword
+     * @return List<UsernameResponseDto>
+     * @author 전지환
+     */
+    @Override
+    public List<UsernameResponseDto> searchUsernameKeywordBased(String keyword) {
+        return queryFactory
+                .select(new QUsernameResponseDto(
+                    memberEntity.username
+                ))
+                .from(memberEntity)
+                .where(memberEntity.username.contains("%"+keyword+"%"))
+                .fetch();
+    }
+}
